6df0222bdb8a14159a0b098e6a20dadb1df22147
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-06-16 01:38:42 +0530

    Time: 0 ms (100%), Space: 11.6 MB (62.15%) - LeetHub

--- 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
+++ 1025-minimum-cost-for-tickets/1025-minimum-cost-for-tickets.cpp
@@ -1,31 +1,27 @@
 class Solution {
 private:
-int solve(vector<int>& days, vector<int>& costs,int index , int n){
-    vector<int> dp(n+1 , INT_MAX);
-    dp[n]=0;
-    // if(index>=n){
-    //     return 0;
-    // }
-    // if(dp[index] != -1){
-    //     return dp[index];
-    // }
-    for(int k=n-1 ; k>=0 ; k--){
-    int day1 = costs[0]+dp[k+1];
-    int i;
-    for(i=k ; i<n && days[i]<days[k]+7;i++);
-    int day7 = costs[1] + dp[i];
-    int j;
-    for(j=k ; j<n&&days[j]<days[k]+30 ; j++);
-    int day30 = costs[2]+dp[j];
-    dp[k]= min(day1,min(day7,day30));
+int solve(vector<int>& days, vector<int>& costs,int index , int n,vector<int> &dp){
+    if(index>=n){
+        return 0;
     }
-    return dp[0];
+    if(dp[index] != -1){
+        return dp[index];
+    }
+    int day1 = costs[0]+solve(days,costs,index+1,n,dp);
+    int i;
+    for(i=index ; i<n && days[i]<days[index]+7;i++);
+    int day7 = costs[1] + solve(days,costs,i,n,dp);
+    int j;
+    for(j=index ; j<n&&days[j]<days[index]+30 ; j++);
+    int day30 = costs[2]+solve(days,costs,j,n,dp);
+    dp[index]= min(day1,min(day7,day30));
+    return dp[index];
 }
 
 public:
     int mincostTickets(vector<int>& days, vector<int>& costs) {
         int n = days.size();
         vector<int> dp(n,-1);
-        return solve(days,costs,0,n);
+        return solve(days,costs,0,n,dp);
     }
 };
