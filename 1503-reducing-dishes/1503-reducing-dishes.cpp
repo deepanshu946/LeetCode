cbb124dd5101760be3b648efde0b668a7277548f
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-06-18 01:51:20 +0530

    Time: 3 ms (83.94%), Space: 10.3 MB (82.68%) - LeetHub

--- 1503-reducing-dishes/1503-reducing-dishes.cpp
+++ 1503-reducing-dishes/1503-reducing-dishes.cpp
@@ -17,6 +17,19 @@ public:
     int maxSatisfaction(vector<int>& satisfaction) {
         int n = satisfaction.size();
         sort(satisfaction.begin() , satisfaction.end());
-        return solve(satisfaction,n);
+        // return solve(satisfaction,n);
+        int ans=0;
+        for(int i=0;i<satisfaction.size();i++){
+           int sum=0;
+           int count=1;
+           for(int j=i;j<satisfaction.size();j++){
+              sum+=satisfaction[j]*count;
+              count++;
+           }
+        ans=max(ans,sum);
+        }
+        return ans;
+
+
     }
 };
