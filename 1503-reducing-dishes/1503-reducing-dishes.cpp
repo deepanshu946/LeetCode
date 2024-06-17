b6e7db19d0d48776b96106ff9b4c59428cc0a6ce
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-06-18 01:46:34 +0530

    Time: 12 ms (48.23%), Space: 10.6 MB (67.66%) - LeetHub

--- 1503-reducing-dishes/1503-reducing-dishes.cpp
+++ 1503-reducing-dishes/1503-reducing-dishes.cpp
@@ -1,13 +1,8 @@
 class Solution { 
 private:
 int solve(vector<int> &s , int n){
-    // vector<vector<int>> dp(n+1,vector<int>(n+2,0));
-    vector<int> nexttime(n+1,0);
-    vector<int> currtime(n+1,0);
     vector<int> currindex(n+2,0);
     vector<int> nextindex(n+2,0);
-
-    
     for(int index = n-1 ; index>=0 ; index--){
         for(int time = n ; time>=1 ; time--){
             int inc = s[index]*(time) + nextindex[time+1];
