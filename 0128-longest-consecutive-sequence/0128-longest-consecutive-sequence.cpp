922d24a29a1397914b34c3db3968861855d75b9c
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-06-19 02:22:09 +0530

    Time: 125 ms (52.96%), Space: 74.3 MB (32.07%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -5,25 +5,45 @@ public:
     int longestConsecutive(vector<int>& nums) {
 
         // map<int,bool> m;
-        set<int> s;
+        // for(int i=0 ; i<nums.size() ; i++){
+        //     m[nums[i]]=1;
+        // }
+        // unordered_map<int,int> m2;
+        // int ans = 0;
+        // for(auto i:m){
+        //     int val = i.first;
+        //     if(m.find(val-1) == m.end()){
+        //         m2[val]=1;
+        //     }
+        //     else{
+        //         //val-1 present in nums
+        //         m2[val]=1+m2[val-1];
+        //     }
+
+        // ans = max(ans,m2[val]);
+
+        // }
+        // return ans;
+
+        unordered_map<int,bool> m;
         for(int i=0 ; i<nums.size() ; i++){
-            // m[nums[i]]=1;
-            s.insert(nums[i]);
+            m[nums[i]]=1;
         }
-        unordered_map<int,int> m2;
+
         int ans = 0;
-        for(auto i:s){
-            int val = i;
-            if(!s.contains(val-1)){
-                m2[val]=1;
-            }
-            else{
-                //val-1 present in nums
-                m2[val]=1+m2[val-1];
-            }
 
-        ans = max(ans,m2[val]);
-
+        for(auto i:m){
+            int val = i.first;
+            // int temp = 0;
+            if(m.find(val-1) == m.end()){
+                int count=1;
+                int temp = val+1;
+                while(m.find(temp) != m.end()){
+                    count++;
+                    temp++;
+                }
+                ans=max(ans,count);
+            }
         }
         return ans;
     }
