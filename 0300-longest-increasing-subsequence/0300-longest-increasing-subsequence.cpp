8b9a3b1954fd539a42e5ce8c964a54a43a49b6f7
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-06-18 19:01:35 +0530

    Time: 6 ms (89.47%), Space: 12.7 MB (82.48%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,30 +1,30 @@
 class Solution {
-private:
-int solve(vector<int> &nums , int n){
-    // vector<vector<int>> dp(n+1 , vector<int>(n+1,0));
-    vector<int> nextrow(n+1 , 0);
-    vector<int> currrow(n+1,0);
-    // if(curr==n){
-    //     return 0;
-    // }
-    // if(dp[curr][prev+1] !=-1){
-    //     return dp[curr][prev+1];
-    // }
-    for(int curr = n-1 ; curr>=0 ; curr--){
-        for(int prev = n-1 ; prev>=-1 ; prev--){
-            int inc =0;
-            int exc=0;
-            if(prev == -1 || nums[curr]>nums[prev]){
-                inc = 1+nextrow[curr+1];
-            }
-            exc = nextrow[prev+1];
-            currrow[prev+1]=max(inc,exc);
-            // return dp[curr][prev];
-        }
-        nextrow = currrow;
-    }
-    return nextrow[0];
-}
+// private:
+// int solve(vector<int> &nums , int n){
+//     // vector<vector<int>> dp(n+1 , vector<int>(n+1,0));
+//     vector<int> nextrow(n+1 , 0);
+//     vector<int> currrow(n+1,0);
+//     // if(curr==n){
+//     //     return 0;
+//     // }
+//     // if(dp[curr][prev+1] !=-1){
+//     //     return dp[curr][prev+1];
+//     // }
+//     for(int curr = n-1 ; curr>=0 ; curr--){
+//         for(int prev = n-1 ; prev>=-1 ; prev--){
+//             int inc =0;
+//             int exc=0;
+//             if(prev == -1 || nums[curr]>nums[prev]){
+//                 inc = 1+nextrow[curr+1];
+//             }
+//             exc = nextrow[prev+1];
+//             currrow[prev+1]=max(inc,exc);
+//             // return dp[curr][prev];
+//         }
+//         nextrow = currrow;
+//     }
+//     return nextrow[0];
+// }
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
