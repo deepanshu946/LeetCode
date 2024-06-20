ac0d75705eef32a481bf9664e5429785574aa20c
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-06-20 18:19:04 +0530

    Time: 26 ms (24.95%), Space: 7.8 MB (83.39%) - LeetHub

--- 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
+++ 1263-number-of-dice-rolls-with-target-sum/1263-number-of-dice-rolls-with-target-sum.cpp
@@ -2,25 +2,24 @@ class Solution {
 #define mod 1000000007
 private:
 int solve(int n , int k , int target){
-    vector<vector<int>> dp(n+1 , vector<int>(target+1,0));
-
-    if(target<0){
-        return 0;
-    }
-    dp[0][0]=1;
+    // vector<vector<int>> dp(n+1 , vector<int>(target+1,0));
+    vector<int> curr(target+1,0);
+    vector<int> prev(target+1 , 0);
+    prev[0]=1;
 
     for(int dice = 1 ; dice<=n ; dice++){
         for(int sum = 1 ; sum <=target ; sum++ ){
             int ans=0 ;
             for(int i=1 ; i<=k ; i++){
                 if(sum-i>=0){
-                    ans = (ans%mod+dp[dice-1][sum-i]%mod)%mod;
+                    ans = (ans%mod+prev[sum-i]%mod)%mod;
                 }
             }
-            dp[dice][sum]=ans%mod;
+            curr[sum]=ans%mod;
         }
+        prev = curr;
     }
-    return dp[n][target];
+    return curr[target];
 
 
     
