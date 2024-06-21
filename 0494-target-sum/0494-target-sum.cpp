8440e41aaddaff191756d00db4b5df0495f9333b
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-06-21 20:32:01 +0530

    Time: 176 ms (39.62%), Space: 59.1 MB (15.68%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,21 +1,26 @@
 class Solution {
 private:
-int solve(vector<int> &nums , int target , int index , int n){
+int solve(vector<int> &nums , int target , int index , int n,unordered_map<int,int> dp[]){
     if(target==0 && index==n){
         return 1;
     }
     if(index>=n){
         return 0;
     }
-    int plus = solve(nums,target-nums[index] , index+1 , n);
-    int minus = solve(nums,target+nums[index],index+1 , n);
-    return plus+minus ;
+    if(dp[index].count(target)){
+        return dp[index][target];
+    }
+    int plus = solve(nums,target-nums[index] , index+1 , n,dp);
+    int minus = solve(nums,target+nums[index],index+1 , n,dp);
+    dp[index][target]=plus+minus;
+    return dp[index][target] ;
 
 }
 
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-        return solve(nums,target,0,n);
+        unordered_map<int,int> dp[n+1];
+        return solve(nums,target,0,n,dp);
     }
 };
