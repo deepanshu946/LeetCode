e3f93a194a0590f338f2af51d714458a8da64cd8
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-06-23 20:20:41 +0530

    Time: 3 ms (92.52%), Space: 9.5 MB (90.57%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,28 +1,34 @@
 class Solution {
 private:
 int solve(string &a , string &b){
-    vector<vector<int>> dp(a.length()+1 , vector<int> (b.length()+1 , 0));
+    // vector<vector<int>> dp(a.length()+1 , vector<int> (b.length()+1 , 0));
+    vector<int> prev(b.length()+1,0);
+    vector<int> curr(b.length()+1,0);
+
     //fill base case
     for(int i=0 ; i<=b.length() ; i++){
-        dp[a.length()][i]=b.length()-i;
+        prev[i]=b.length()-i;
     }
-    for(int i=0 ; i<=a.length() ; i++){
-        dp[i][b.length()]=a.length()-i;
-    }
+    int k=1;
+    curr[b.length()]=k;
     for(int i=a.length()-1 ; i>=0 ; i--){
+
         for(int j=b.length()-1 ; j>=0 ; j--){
             if(a[i]==b[j]){
-                dp[i][j]= dp[i+1][j+1];
+                curr[j]= prev[j+1];
             }
             else{
-                int ins = 1+dp[i][j+1];
-                int del = 1+dp[i+1][j];
-                int rep= 1+dp[i+1][j+1];
-                dp[i][j]= min(ins,min(del,rep));
+                int ins = 1+curr[j+1];
+                int del = 1+prev[j];
+                int rep= 1+prev[j+1];
+                curr[j]= min(ins,min(del,rep));
             }
         }
+        k++;
+        prev = curr;
+        curr[b.length()]=k;
     }
-    return dp[0][0];
+    return prev[0];
 }
 public:
     int minDistance(string word1, string word2) {
