07aa867242c52774871d154643a520c1f390ebca
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-06-26 01:12:57 +0530

    Time: 8 ms (27.32%), Space: 11.1 MB (76.28%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,15 +1,18 @@
 class Solution {
 private:
 int solve(vector<vector<int>>&tri){
-    vector<vector<int>> dp(tri.size()+1 , vector<int>(tri.back().size()+1 , 0));
+    // vector<vector<int>> dp(tri.size()+1 , vector<int>(tri.back().size()+1 , 0));
+    vector<int> curr(tri.back().size()+1 , 0);
+    vector<int> prev(tri.back().size()+1 , 0);
     for(int row = tri.size()-1 ; row>=0 ; row--){
         for(int index = row ; index>=0 ; index--){
-            int same = dp[row+1][index];
-            int plus = dp[row+1][index+1];
-            dp[row][index]=tri[row][index] + min(same,plus);
+            int same = prev[index];
+            int plus = prev[index+1];
+            curr[index]=tri[row][index] + min(same,plus);
         }
+        prev = curr;
     }
-    return dp[0][0];
+    return prev[0];
     
 }
 public:
