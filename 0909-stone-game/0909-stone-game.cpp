2cb0b59346c978f17261b9bdcd988cacb6fea03f
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-06-26 23:27:04 +0530

    Time: 234 ms (6.08%), Space: 118.2 MB (5.12%) - LeetHub

--- 0909-stone-game/0909-stone-game.cpp
+++ 0909-stone-game/0909-stone-game.cpp
@@ -1,5 +1,47 @@
 class Solution {
-    int solve(vector<int>& piles, int start, int end, bool alice,vector<vector<vector<int>>> &dp) {
+    int solve(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<vector<int>>> dp(n,vector<vector<int>>(n,vector<int> (2 ,0)));
+
+        for(int start=n-1 ; start>=0 ; start--){
+            for(int end =start+1 ; end<n ; end++ ){
+                for(int alice=1 ; alice>=0; alice--){
+                    int ans1 = INT_MIN;
+                    int ans2 = INT_MIN;
+                    if (alice) {
+                        ans1 = piles[start] + dp[start+1][end][0];
+                        ans2 = piles[end] + dp[start][end-1][0];
+
+                    } else {
+                        ans1 = dp[start+1][end][1];
+                        ans2 = dp[start][end-1][1];
+                    }
+                    dp[start][end][alice] = max(ans1, ans2);
+                }
+            }
+        }
+        return dp[0][n-1][1];
+    }
+
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum = sum + piles[i];
+        }
+        int alice = solve(piles);
+        if (alice > sum / 2) {
+            return true;
+        }
+        return false;
+    }
+};
+
+
+//rec + memo
+int solver(vector<int>& piles, int start, int end, bool alice,vector<vector<vector<int>>> &dp) {
         if (start >= end) {
             return 0;
         }
@@ -9,33 +51,16 @@ class Solution {
         int ans1 = INT_MIN;
         int ans2 = INT_MIN;
         if (alice) {
-            ans1 = piles[start] + solve(piles, start + 1, end, 0,dp);
+            ans1 = piles[start] + solver(piles, start + 1, end, 0,dp);
         } else {
-            ans1 = solve(piles, start + 1, end, 1,dp);
+            ans1 = solver(piles, start + 1, end, 1,dp);
         }
         if (alice) {
-            ans2 = piles[end] + solve(piles, start, end - 1, 0,dp);
+            ans2 = piles[end] + solver(piles, start, end - 1, 0,dp);
         } else {
-            ans2 = solve(piles, start, end - 1, 1,dp);
+            ans2 = solver(piles, start, end - 1, 1,dp);
         }
 
         dp[start][end][alice] = max(ans1, ans2);
         return dp[start][end][alice];
     }
-
-public:
-    bool stoneGame(vector<int>& piles) {
-        int n = piles.size();
-        vector<vector<vector<int>>> dp(n+1 , vector<vector<int>>(n+1 , vector<int> ( 2 ,-1)));
-
-        int sum = 0;
-        for (int i = 0; i < n; i++) {
-            sum = sum + piles[i];
-        }
-        int alice = solve(piles, 0, n - 1, 1,dp);
-        if (alice > sum / 2) {
-            return true;
-        }
-        return false;
-    }
-};
