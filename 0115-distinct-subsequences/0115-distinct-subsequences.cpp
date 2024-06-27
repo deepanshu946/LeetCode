9ef773e1dc70d5031dca426f8932ec5bd03316b8
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-06-27 20:00:20 +0530

    Time: 18 ms (61.97%), Space: 7.6 MB (91.76%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ 0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -2,22 +2,23 @@ class Solution {
 private:
 #define mod 1000000007
 int solve(string s , string t ){
-    vector<vector<int>> dp(s.length()+1 , vector<int> (t.length()+1,0));
-    for(int i=0 ; i<=s.length() ; i++){
-        dp[i][t.length()]=1;
-    }
-
+    // vector<vector<int>> dp(s.length()+1 , vector<int> (t.length()+1,0));
+    vector<int> prev(t.length()+1 , 0);
+    vector<int> curr(t.length()+1 , 0);
+    curr[t.length()]=1;
+    prev[t.length()]=1;
     for(int i=s.length()-1; i>=0 ; i--){
         for(int j=t.length()-1 ; j>=0 ; j--){
             int inc = 0;
             if(s[i]==t[j]){
-                inc = (dp[i+1][j+1])%mod;
+                inc = (prev[j+1])%mod;
             }
-            int exc = (dp[i+1][j])%mod;
-            dp[i][j]= (inc+exc)%mod;
+            int exc = (prev[j])%mod;
+            curr[j]= (inc+exc)%mod;
         }
+        prev = curr;
     }
-    return dp[0][0];
+    return prev[0];
 
 
 }
