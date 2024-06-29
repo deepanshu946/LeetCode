654fa9085c0d893938c1ffca9adeba3dbb369ab3
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-06-30 00:59:26 +0530

    Time: 0 ms (100%), Space: 7.4 MB (97.03%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.cpp
+++ 0091-decode-ways/0091-decode-ways.cpp
@@ -1,25 +1,29 @@
 class Solution {
 private:
 int solve(string& s){
-    vector<int> dp(s.length()+2 , 0);
-    dp[s.length()]=1;
+    // vector<int> dp(s.length()+2 , 0);
+    int next2=0;
+    int next1=1;
+
+    // dp[s.length()]=1;
     for(int i=s.length()-1 ; i>=0 ; i--){
         int ans1=0;
         int ans2=0;
         char digit1 = s[i];
         int num = digit1-48;
         if(num != 0){
-            ans1 = dp[i+1];
+            ans1 = next1;
             char digit2 = s[i+1];
             num=10*num+(digit2-48);
             if(num>0 && num<=26){
-                ans2 = dp[i+2];
+                ans2 = next2;
             }
         }
-        dp[i]= ans1+ans2;
-
+        int curr= ans1+ans2;
+        next2=next1;
+        next1=curr;
     }
-    return dp[0];
+    return next1;
     
 
 }
