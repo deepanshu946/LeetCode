97b16fcf8cb07b7670ea80d288005f154315230e
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-06-30 17:19:01 +0530

    Time: 2885 ms (5%), Space: 44.5 MB (24.44%) - LeetHub

--- 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
+++ 0853-most-profit-assigning-work/0853-most-profit-assigning-work.cpp
@@ -1,14 +1,33 @@
 class Solution {
 public:
     int maxProfitAssignment(vector<int>& difficulty, vector<int>& profit, vector<int>& worker) {
-        // sort(profit.begin() , profit.end());
+        map<int,int> m;
+        for(int i=0 ; i<profit.size() ; i++){
+            if(m.find(difficulty[i]) != m.end()){
+            m[difficulty[i]]=max(m[difficulty[i]],profit[i]);
+
+            }
+            else{
+                m[difficulty[i]]=profit[i];
+
+            }
+        }
+
         int ans = 0;
         for(int i=0 ; i<worker.size() ; i++){
             int prof=0;
             int work = worker[i];
-            for(int j=0 ; j<profit.size() ; j++){
-                if(work>=difficulty[j]){
-                    prof = max(prof,profit[j]);
+            // for(int j=0 ; j<profit.size() ; j++){
+            //     if(work>=difficulty[j]){
+            //         prof = max(prof,profit[j]);
+            //     }
+            // }
+            for(auto k:m){
+                if(k.first>work){
+                    break;
+                }
+                if(work>=k.first){
+                    prof= max(prof,k.second);
                 }
             }
             ans = ans+prof;
