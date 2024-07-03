504acb3d54ef6b3ef83d2af530f4861c5e966ef0
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-07-04 01:35:23 +0530

    Time: 4 ms (55.47%), Space: 13.6 MB (76.87%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,7 +2,10 @@ class Solution {
 private:
 //recursion
 int solve( vector<int> prices , int n){
-    vector<vector<int>> dp(n+2 , vector<int> ( 2 , 0));
+    // vector<vector<int>> dp(n+2 , vector<int> ( 2 , 0));
+    vector<int> next1(2,0);
+    vector<int> next2(2,0);
+    vector<int> curr(2,0);
     // if(i>=n){
     //     return 0;
     // }
@@ -10,15 +13,17 @@ int solve( vector<int> prices , int n){
         for(int buy=0 ; buy<=1 ; buy++){
             int profit = 0;
             if(buy){
-                profit=max(-prices[i]+dp[i+1][0] , dp[i+1][1]);
+                profit=max(-prices[i]+next1[0] , next1[1]);
             }
             else{
-                profit = max(prices[i]+dp[i+2][1],dp[i+1][0]);
+                profit = max(prices[i]+next2[1],next1[0]);
             }
-            dp[i][buy]= profit;
+            curr[buy]= profit;
         }
+        next2 = next1;
+        next1 = curr;
     }
-    return dp[0][1];
+    return next1[1];
 }
 public:
     int maxProfit(vector<int>& prices) {
