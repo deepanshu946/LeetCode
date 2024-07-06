af784e4ade1533bfd9c674f938bf6a2130729410
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-07-06 19:39:26 +0530

    Time: 2 ms (54.14%), Space: 7 MB (71.18%) - LeetHub

--- 2645-pass-the-pillow/2645-pass-the-pillow.cpp
+++ 2645-pass-the-pillow/2645-pass-the-pillow.cpp
@@ -3,7 +3,6 @@ public:
     int passThePillow(int n, int time) {
         int dxn = time / (n-1);
         int left = time % (n-1);
-        // int a = abs(time-n);
         if(dxn%2 ==0){
             return left+1;
         }
