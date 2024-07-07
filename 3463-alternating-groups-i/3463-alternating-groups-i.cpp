9cb4e4b68e202389ec85e94a173973e5179cf12c
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-07-07 20:19:10 +0530

    Time: 17 ms (40%), Space: 25.9 MB (20%) - LeetHub

--- 3463-alternating-groups-i/3463-alternating-groups-i.cpp
+++ 3463-alternating-groups-i/3463-alternating-groups-i.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     int numberOfAlternatingGroups(vector<int>& nums) {
         int n = nums.size();
-        int ans=0;
-        for(int i=0 ; i<n ; i++){
-            if(i==0){
-                if(nums[1] != nums[0] && nums[n-1] != nums[0] && nums[n-1]==nums[1]){
-                    ans++;
-                }
-                
-
+        int ans = 0;
+        for (int i = 1; i < n - 1; i++) {
+            if (nums[i + 1] != nums[i] && nums[i - 1] != nums[i] &&
+                nums[i - 1] == nums[i + 1]) {
+                ans++;
             }
-            else if(i==n-1){
-                    if(nums[0] != nums[n-1] && nums[n-2] != nums[n-1] && nums[n-2]==nums[0]){
-                    ans++;
-                } 
-
-            }
-            else{
-                if(nums[i+1] != nums[i] && nums[i-1] != nums[i] && nums[i-1]==nums[i+1]){
-                    ans++;
-                }
-            }
+        }
+        if (nums[1] != nums[0] && nums[n - 1] != nums[0] &&
+            nums[n - 1] == nums[1]) {
+            ans++;
+        }
+        if (nums[0] != nums[n - 1] && nums[n - 2] != nums[n - 1] &&
+            nums[n - 2] == nums[0]) {
+            ans++;
         }
         return ans;
     }
