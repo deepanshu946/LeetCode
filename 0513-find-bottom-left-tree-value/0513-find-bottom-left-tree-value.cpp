e4fe7e53f90996911f77fe3de39d30c6a99f3341
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-07-11 12:28:58 +0530

    Time: 18 ms (22.43%), Space: 23 MB (87.09%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.cpp
@@ -11,31 +11,45 @@
  */
 class Solution {
 private:
+int curr = -1;
+void solve(TreeNode* root , int &ans ,int level){
+    if(root==NULL){
+        return;
+    }
+    if(level>curr){
+        ans = root->val;
+        curr=level;
+    }
+    solve(root->left,ans,level+1);
+    solve(root->right,ans,level+1);
+    
+}
 public:
     int findBottomLeftValue(TreeNode* root) {
         int ans = 0;
         int level = 0;
-        // solve(root,ans,level,0);
-        queue<TreeNode*> q;
-        q.push(root);
-        while(!q.empty()){
-            int size = q.size();
-            bool check = 0;
-            for(int i=0 ; i<size ; i++){
-                TreeNode* front = q.front();
-                q.pop();
-                if(!check){
-                    check = 1;
-                    ans = front->val;
-                }
-                if(front->left){
-                    q.push(front->left);
-                }
-                if(front->right){
-                    q.push(front->right);
-                }
-            }
-        }
+        solve(root,ans,level);
         return ans;
+        // queue<TreeNode*> q;
+        // q.push(root);
+        // while(!q.empty()){
+        //     int size = q.size();
+        //     bool check = 0;
+        //     for(int i=0 ; i<size ; i++){
+        //         TreeNode* front = q.front();
+        //         q.pop();
+        //         if(!check){
+        //             check = 1;
+        //             ans = front->val;
+        //         }
+        //         if(front->left){
+        //             q.push(front->left);
+        //         }
+        //         if(front->right){
+        //             q.push(front->right);
+        //         }
+        //     }
+        // }
+        // return ans;
     }
 };
