e7be09e8c1cbd3bc4c4431e29c368861d0370a1c
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-07-17 02:04:57 +0530

    Time: 0 ms (100%), Space: 8.6 MB (7.76%) - LeetHub

--- 3312-number-of-changing-keys/3312-number-of-changing-keys.cpp
+++ 3312-number-of-changing-keys/3312-number-of-changing-keys.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int countKeyChanges(string s) {
-        for(int i=0 ; i<s.length() ; i++){
+        if(s[0]>='A' && s[0]<='Z'){
+                s[0]=s[0]-'A'+'a';
+            }
+        int ans =0;
+
+        for(int i=1 ; i<s.length() ; i++){
             if(s[i]>='A' && s[i]<='Z'){
                 s[i]=s[i]-'A'+'a';
             }
-        }
-        int ans =0;
-        for(int i=1 ; i<s.length() ; i++){
             if(s[i] != s[i-1]){
                 ans++;
             }
         }
+        
         return ans;
 
     }
