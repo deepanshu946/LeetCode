c1188748b5ebb3fc91e47f37999c3c0df4fb2cf5
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-07-29 02:37:59 +0530

    Time: 98 ms (48.06%), Space: 30.8 MB (50.28%) - LeetHub

--- 3414-find-number-of-ways-to-reach-the-k-th-stair/3414-find-number-of-ways-to-reach-the-k-th-stair.cpp
+++ 3414-find-number-of-ways-to-reach-the-k-th-stair/3414-find-number-of-ways-to-reach-the-k-th-stair.cpp
@@ -1,23 +1,31 @@
 class Solution {
-public:
-        
-    int solve(int i, int jump, int back, map<array<int, 3>, int>& dp, int k) {
-        if(i > k + 1) {
+private:
+    map<array<int,3> , int> m;
+    int solve(int k, int i, int jump, int done) {
+        if (i > k + 1 || i < 0) {
             return 0;
         }
-        if(dp.count({i, jump, back})) {
-            return dp[{i, jump, back}];
+        if (i<k+2 && jump<k+1 && m.find({i,jump,done}) != m.end()) {
+            return m[{i,jump,done}];
         }
-        int ans = (i == k);
-        if(back == 1 && i > 0) {
-            ans += solve(i - 1, jump, 0, dp, k);
+        int ans = 0;
+        if(i==k){
+            ans++;
         }
-        ans += solve(i + pow(2, jump), jump + 1, 1, dp, k);
-        return dp[{i, jump, back}] = ans;
+        if (done == 0) {
+            ans += solve(k, i - 1, jump, 1);
+        }
+        ans += solve(k, i + pow(2,jump), jump + 1, 0);
+        if(i<k+2 && jump<k+1){
+            return m[{i,jump,done}]= ans;
+        }
+        else{
+            return ans;
+        }
     }
-    
+
+public:
     int waysToReachStair(int k) {
-        map<array<int, 3>, int> dp; 
-        return solve(1, 0, 1, dp, k);
+        return solve(k, 1, 0, 0);
     }
 };
