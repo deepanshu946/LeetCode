3b7d3706dcb52ef4852e67d4a95cbf9bca878e12
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-07-31 02:38:44 +0530

    Time: 430 ms (5.03%), Space: 20.4 MB (15.23%) - LeetHub

---
+++ 2576-minimum-penalty-for-a-shop/2576-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int bestClosingTime(string nums) {
+        int n = nums.length();
+        vector<int> temp(n+1,0);
+        for(int i=1 ; i<=n ; i++){
+            if(nums[i-1]=='N'){
+                temp[i]=temp[i-1]+1;
+            }
+            else{
+                temp[i]=temp[i-1];
+            }
+            
+        }
+        vector<int> temp2(n+1,0);
+        int ans = temp[n]+temp2[n] ;
+        int time = n;
+
+        for(int i=n-1 ; i>=0 ; i--){
+            if(nums[i]=='Y'){
+                temp2[i]=temp2[i+1]+1;
+            }
+            else{
+                temp2[i]=temp2[i+1];
+            }
+        if(temp[i]+temp2[i] <= ans){
+            time = i;
+            ans = temp[i]+temp2[i];
+        }
+
+        }
+        for(int i=0 ; i<=n ; i++){
+            cout<<temp[i]<<" ";
+        }
+        cout<<endl;
+        for(int i=0 ; i<=n ; i++){
+            cout<<temp2[i]<<" ";
+        }
+        return time;
+
+    }
+};
