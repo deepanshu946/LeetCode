2d2c894789bc0544e7528f51c90e748b398d3125
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-08-08 01:17:46 +0530

    Time: 11 ms (99.11%), Space: 8.2 MB (89.04%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.cpp
@@ -5,18 +5,21 @@ int solve( int n , string &s){
     //     return 0;
     // }
     
-    vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    // vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+    vector<int> curr(n+1,0);
+    vector<int> prev(n+1,0);
     for(int start = n-2 ; start>=0 ; start--){
         for(int end = start+1 ; end < n ; end++){
         if(s[start]==s[end]){
-            dp[start][end]= dp[start+1][end-1];
+            prev[end]= curr[end-1];
             }
             else{
-                dp[start][end]= min(1+dp[start+1][end] , 1+dp[start][end-1]);
+                prev[end]= min(1+curr[end] , 1+prev[end-1]);
             }
         }
+        curr = prev;
     }
-    return dp[0][n-1];
+    return curr[n-1];
 
 }
 public:
