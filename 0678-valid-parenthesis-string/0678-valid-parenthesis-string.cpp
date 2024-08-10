33ae7571d2253d1402b1dda9ffa08baff89f11a0
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-08-10 17:01:24 +0530

    Time: 3 ms (51.77%), Space: 10.7 MB (8.15%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.cpp
@@ -2,29 +2,45 @@ class Solution {
 private:
 int dp[102][102];
 bool solve(string s , int index , int left){
-    if(left<0){
-        return false;
+    // if(left<0){
+    //     return false;
+    // }
+    // if(left==0 && index==s.length()){
+    //     return true;
+    // }
+    // if(left!=0 && index==s.length()){
+    //     return false;
+    // }
+    // if(dp[index][left] != -1){
+    //     return dp[index][left];
+    // }
+    vector<vector<int>> dp(s.length()+1 , vector<int>(s.length()+1,0));
+    dp[s.length()][0]=1;
+    for(int index=s.length()-1 ; index>=0 ; index--){
+        for(int left=s.length()-1 ; left>=0 ; left--){
+            bool ans = 0;
+            if(s[index]=='('){
+            ans = ans | dp[index+1][left+1];
+            }
+            if(s[index]==')'){
+                if(left-1>=0)
+                    ans = ans | dp[index+1][left-1];
+            }
+            if(s[index]=='*'){
+                
+                ans = ans | dp[index+1][left+1] ;
+                if(left-1>=0)
+                    ans = ans | dp[index+1][left-1] ;
+                ans = ans | dp[index+1][left];
+            }
+             dp[index][left]=ans;
+        }
     }
-    if(left==0 && index==s.length()){
-        return true;
-    }
-    if(left!=0 && index==s.length()){
-        return false;
-    }
-    if(dp[index][left] != -1){
-        return dp[index][left];
-    }
-    bool ans = 0;
-    if(s[index]=='('){
-       ans = ans | solve(s,index+1,left+1);
-    }
-    if(s[index]==')'){
-        ans = ans | solve(s,index+1,left-1);
-    }
-    if(s[index]=='*'){
-        ans = ans | solve(s,index+1,left+1) | solve(s,index+1,left-1) | solve(s,index+1,left);
-    }
-    return dp[index][left]= ans;
+    return dp[0][0];
+
+
+
+    
 }
 public:
     bool checkValidString(string s) {
