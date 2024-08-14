1646ec32e452e2277cfbd192ff99ec1cc76a352f
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-08-14 21:25:34 +0530

    Time: 122 ms (48.15%), Space: 94.6 MB (11.73%) - LeetHub

--- 0769-largest-plus-sign/0769-largest-plus-sign.cpp
+++ 0769-largest-plus-sign/0769-largest-plus-sign.cpp
@@ -1,52 +1,25 @@
 class Solution {
 public:
     int orderOfLargestPlusSign(int n, vector<vector<int>>& mines) {
-        set<pair<int,int>> s;
-        for(int i=0 ; i<mines.size() ; i++){
-            s.insert({mines[i][0],mines[i][1]});
+        vector<vector<int>> arr(n,vector<int>(n,1)),up,dn,l,r;
+        for(auto it:mines) arr[it[0]][it[1]]=0;
+        dn=l=r=up=arr;
+        for(int i=1;i<n;i++){
+            for(int j=0;j<n;j++) if(arr[i][j]!=0) up[i][j]+=up[i-1][j];
         }
-        vector<vector<int>> dp(n,vector<int>(n,0));
-        for(int i=n-2 ; i>=0 ; i--){
-            for(int j=0 ; j<n ; j++){
-                if(s.count({i,j}) == 0){
-                    dp[i][j]=!s.count({i+1,j})+dp[i+1][j];
-                }
-            }
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<n;j++) if(arr[i][j]!=0) dn[i][j]+=dn[i+1][j];
         }
-        vector<vector<int>> dp2(n,vector<int>(n,0));
-        for(int i=0 ; i<n ; i++){
-            for(int j=1 ; j<n ; j++){
-                if(s.count({i,j}) == 0){
-                    dp2[i][j]=dp2[i][j-1] + !s.count({i,j-1}) ;
-                }
-            }
+        for(int j=1;j<n;j++){
+            for(int i=0;i<n;i++) if(arr[i][j]!=0) r[i][j]+=r[i][j-1];
         }
-        vector<vector<int>> dp3(n,vector<int>(n,0));
-        for(int i=1 ; i<n ; i++){
-            for(int j=0 ; j<n ; j++){
-                if(s.count({i,j}) == 0){
-                    dp3[i][j]=dp3[i-1][j]+ !s.count({i-1,j}) ;
-                }
-            }
+        for(int j=n-2;j>=0;j--){
+            for(int i=0;i<n;i++) if(arr[i][j]!=0) l[i][j]+=l[i][j+1];
         }
-        vector<vector<int>> dp4(n,vector<int>(n,0));
-        for(int i=0 ; i<n ; i++){
-            for(int j=n-2 ; j>=0 ; j--){
-                if(s.count({i,j}) == 0){
-                    dp4[i][j]=dp4[i][j+1] + !s.count({i,j+1}) ;
-                }
-            }
-        }
-        int ans = 0;
-        for(int i=0 ; i<n ; i++){
-            for(int j=0 ; j<n ; j++){
-                if(s.count({i,j})==0){
-                    ans = max(ans , 1+min(dp[i][j],min(dp2[i][j], min(dp3[i][j],dp4[i][j]))));
-                }
-            }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++) ans=max(ans,min({up[i][j],dn[i][j],l[i][j],r[i][j]}));
         }
         return ans;
-
-        
     }
 };
