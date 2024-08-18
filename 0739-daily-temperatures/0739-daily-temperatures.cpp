4c229f316ca7a0b3fbfd42b2d45113f9b64f3c20
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-08-18 21:09:47 +0530

    Time: 117 ms (64.63%), Space: 101.4 MB (69.46%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,24 +1,46 @@
+// class Solution {
+// public:
+//     vector<int> dailyTemperatures(vector<int>& temperatures) {
+//         int n = temperatures.size();
+//         vector<int> ans(n);
+//         stack<pair<int,int>> s;
+//         s.push({0,0});
+//         int index = n-1;
+//         for(index ; index>=0 ; index--){
+//             int val = temperatures[index];
+//             while(s.top().first != 0 && s.top().first <= val){
+//                 s.pop();
+//             }
+//             if(s.top().first ==0 ){
+//                 ans[index]=0;
+//             }
+//             else{
+//                 ans[index]=s.top().second - index;
+//             }
+//             s.push(make_pair(val,index));
+//         }
+//         return ans;
+//     }
+// };
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n = temperatures.size();
-        vector<int> ans(n);
-        stack<pair<int,int>> s;
-        s.push({0,0});
-        int index = n-1;
-        for(index ; index>=0 ; index--){
-            int val = temperatures[index];
-            while(s.top().first != 0 && s.top().first <= val){
-                s.pop();
+        int n=temperatures.size();
+        vector<int> ans(n,0);
+        stack<int> st;
+        // st.push(n-1);
+        for(int i=temperatures.size()-1;i>=0;i--)
+        {
+            while(!st.empty() && temperatures[i] >= temperatures[st.top()])
+            {
+                st.pop();
             }
-            if(s.top().first ==0 ){
-                ans[index]=0;
+            if(!st.empty()){
+                ans[i]=st.top() - i;
             }
-            else{
-                ans[index]=s.top().second - index;
-            }
-            s.push(make_pair(val,index));
+            st.push(i);
         }
+     
         return ans;
     }
 };
