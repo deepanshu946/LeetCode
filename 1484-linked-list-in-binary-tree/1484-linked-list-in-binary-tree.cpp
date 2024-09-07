ff91387271cf3508e9398354a1fc7f392b8c3d8b
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-09-07 22:03:23 +0530

    Time: 25 ms (55.03%), Space: 30.7 MB (74.94%) - LeetHub

--- 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.cpp
+++ 1484-linked-list-in-binary-tree/1484-linked-list-in-binary-tree.cpp
@@ -1,62 +1,22 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
-/**
- * Definition for a binary tree node.
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- * };
- */
 class Solution {
-private:
-bool check(ListNode* head , TreeNode* root){
-    if(head->next==NULL && root->val==head->val){
-        return 1;
-    }
-    if(root==NULL){
-        return 0;
-    }
-    if(head->val != root->val ){
-        return 0;
-    }
-    // ListNode* temp = head;
-    // TreeNode* roottemp = root;
-    bool ans = 0;
-        
-        if( root->right != NULL){
-            ans = ans | check(head->next,root->right);
-        }
-        if(root->left != NULL){
-            ans = ans | check(head->next,root->left);
-        }
-        return ans;
-    }
-bool gethead(ListNode* head , TreeNode* root){
-    if(root==NULL){
-        return 0;
-    }
-    bool ans = 0;
-    if(head->val == root->val){
-        ans = ans | check(head,root);
-    }
-    ans = ans | gethead(head , root->left);
-    ans = ans | gethead(head,root->right);
-    return ans;
-}
 public:
-    bool isSubPath(ListNode* head, TreeNode* root){
-        return gethead(head , root);
+    bool solve(ListNode*head,TreeNode*root){
+        ListNode*temp=head;
+        if(temp->next==NULL) return true;
+        if(root==NULL) return false;
+        bool ans1=false,ans2=false;
+        if(temp!=NULL && root->val==temp->val){
+            temp=temp->next;
+            if(temp && root->left && temp->val==root->left->val)
+                ans1=solve(temp,root->left);
+            if(temp && root->right && temp->val==root->right->val)
+                ans2=solve(temp,root->right);
+        }
+        return ans1 || ans2;
     }
+    bool isSubPath(ListNode* head, TreeNode* root) {
+        if(root==NULL) return false;
+        if(solve(head,root)) return true;
+        return isSubPath(head,root->left) || isSubPath(head,root->right);
+    }
 };
