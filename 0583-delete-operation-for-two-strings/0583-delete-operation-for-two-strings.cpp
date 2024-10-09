0df82dcefc72d6b557558d71d0485c7dbd155fde
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-10-09 23:13:36 +0530

    Time: 10 ms (73.47%), Space: 11 MB (98.08%) - LeetHub

--- 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
+++ 0583-delete-operation-for-two-strings/0583-delete-operation-for-two-strings.cpp
@@ -1,24 +1,33 @@
 class Solution {
 private:
 int solve( int len1 , int len2 , string &s1 , string &s2){
-    vector<vector<int>> dp(len1+1,vector<int>(len2+1,0));
+    // vector<vector<int>> dp(len1+1,vector<int>(len2+1,0));
+    // for(int j=0 ; j<=len2 ; j++){
+    //     dp[len1][j]=len2-j;
+    // }
+    // for(int i=0 ; i<=len1 ; i++){
+    //     dp[i][len2]=len1-i;
+    // }
+    vector<int> next(len2+1,0);
+    vector<int> prev(len2+1,0);
     for(int j=0 ; j<=len2 ; j++){
-        dp[len1][j]=len2-j;
+        next[j]=len2-j;
     }
-    for(int i=0 ; i<=len1 ; i++){
-        dp[i][len2]=len1-i;
-    }
+    
+
     for(int i=len1-1 ; i>=0 ; i--){
+        prev[len2]=len1-i;
         for(int j=len2-1 ; j>=0 ; j--){
             if(s1[i]==s2[j]){
-                dp[i][j]= dp[i+1][j+1];
+                prev[j]= next[j+1];
             }
             else{
-                dp[i][j]= 1+min(dp[i+1][j],dp[i][j+1]);
+                prev[j]= 1+min(next[j],prev[j+1]);
             }
         }
+        next = prev;
     }
-    return dp[0][0];
+    return prev[0];
 }
 public:
     int minDistance(string word1, string word2) {
