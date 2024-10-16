933d01188cc821dba5408eadb268583f966c3ba5
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-10-16 16:00:00 +0530

    Time: 2 ms (46.69%), Space: 7.9 MB (17.06%) - LeetHub

---
+++ 1304-longest-happy-string/1304-longest-happy-string.cpp
@@ -0,0 +1,55 @@
+class Solution {
+public:
+    class compare{
+        public:
+        bool operator()(pair<int,int> &p1 , pair<int,int> &p2){
+        return p1.first < p2.first; 
+        }
+    };
+    string longestDiverseString(int a, int b, int c) {
+        priority_queue<pair<int,int> ,vector<pair<int,int>> ,  compare> pq;
+        pq.push({a,'a'});
+        pq.push({b,'b'});
+        pq.push({c,'c'});
+        string ans;
+        while(true){
+            pair<int,int> p = pq.top();
+            pq.pop();
+            pair<int,int> p2 = pq.top();
+            pq.pop();
+            if(p.first ==0 || p2.first==0){
+                pq.push(p);
+                pq.push(p2);
+                break;
+            }
+            ans.push_back(p.second);
+            p.first--;
+            if(p.first>0){
+            ans.push_back(p.second);
+            p.first--;
+            }
+            if(p2.first >= p.first && p2.first > 1){
+                ans.push_back(p2.second);
+                ans.push_back(p2.second);
+                p2.first = p2.first-2;
+            }
+            else{
+                ans.push_back(p2.second);
+                p2.first = p2.first-1;
+            }
+            pq.push(p);
+            pq.push(p2);
+            cout<<ans<<endl;
+
+
+        }
+        pair<int,int> p = pq.top();
+        if(p.first != 0 ){
+            ans.push_back(p.second);
+            if(p.first>1){
+                ans.push_back(p.second);
+            }
+        }
+        return ans;
+    }
+};
