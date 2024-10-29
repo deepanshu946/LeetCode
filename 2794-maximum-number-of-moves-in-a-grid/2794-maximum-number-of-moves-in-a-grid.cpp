fdc55044a7438c82a86062778a3810f2d087af93
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-10-29 16:14:29 +0530

    Time: 7 ms (89.13%), Space: 67.7 MB (92.56%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -24,28 +24,32 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         int ans = 0;
-        vector<vector<int>> dp(m,vector<int>(n,0));
+        // vector<vector<int>> dp(m,vector<int>(n,0));
         // for(int i=0 ; i<m ; i++){
         //     ans = max(ans , solve(i,0,m,n,grid,dp));
         // }
         // return ans;
+        vector<int> curr(m,0);
+        vector<int> prev(m,0);
+
         for(int j=n-2 ; j>=0 ;j--){
             for(int i=0 ; i<m ; i++){
                 int ans = 0;
                 if(i-1 >=0 && j+1<n && grid[i-1][j+1] > grid[i][j]){
-                    ans = max(ans , 1+dp[i-1][j+1]);
+                    ans = max(ans , 1+curr[i-1]);
                 }
                 if(j+1<n && grid[i][j+1] > grid[i][j]){
-                    ans = max(ans , 1+dp[i][j+1]);
+                    ans = max(ans , 1+curr[i]);
                 }
                 if(i+1<m && j+1<n && grid[i+1][j+1] > grid[i][j]){
-                    ans = max(ans , 1+dp[i+1][j+1]);
+                    ans = max(ans , 1+curr[i+1]);
                 }
-                dp[i][j]= ans;
+                prev[i]= ans;
             }
+            curr = prev;
         }
         for(int i=0 ; i<m ; i++){
-            ans = max(ans , dp[i][0]);
+            ans = max(ans , curr[i]);
         }
         return ans;
 
