d47348266a1756f6d37ff6f5f79e2ca824489ac5
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-10-29 16:12:34 +0530

    Time: 34 ms (32.61%), Space: 74.3 MB (57.28%) - LeetHub

--- 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
+++ 2794-maximum-number-of-moves-in-a-grid/2794-maximum-number-of-moves-in-a-grid.cpp
@@ -24,10 +24,30 @@ public:
         int m = grid.size();
         int n = grid[0].size();
         int ans = 0;
-        vector<vector<int>> dp(m,vector<int>(n,-1));
+        vector<vector<int>> dp(m,vector<int>(n,0));
+        // for(int i=0 ; i<m ; i++){
+        //     ans = max(ans , solve(i,0,m,n,grid,dp));
+        // }
+        // return ans;
+        for(int j=n-2 ; j>=0 ;j--){
+            for(int i=0 ; i<m ; i++){
+                int ans = 0;
+                if(i-1 >=0 && j+1<n && grid[i-1][j+1] > grid[i][j]){
+                    ans = max(ans , 1+dp[i-1][j+1]);
+                }
+                if(j+1<n && grid[i][j+1] > grid[i][j]){
+                    ans = max(ans , 1+dp[i][j+1]);
+                }
+                if(i+1<m && j+1<n && grid[i+1][j+1] > grid[i][j]){
+                    ans = max(ans , 1+dp[i+1][j+1]);
+                }
+                dp[i][j]= ans;
+            }
+        }
         for(int i=0 ; i<m ; i++){
-            ans = max(ans , solve(i,0,m,n,grid,dp));
+            ans = max(ans , dp[i][0]);
         }
         return ans;
+
     }
 };
