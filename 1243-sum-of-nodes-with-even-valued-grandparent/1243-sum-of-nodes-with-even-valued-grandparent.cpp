7325bc0aaaa9f2978e193bac6dce1648b05f400e
deepanshu946 <deepanshuagarwal946@gmail.com>
2024-11-03 02:37:07 +0530

    Time: 0 ms (100%), Space: 40.1 MB (59.76%) - LeetHub

--- 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.cpp
+++ 1243-sum-of-nodes-with-even-valued-grandparent/1243-sum-of-nodes-with-even-valued-grandparent.cpp
@@ -6,44 +6,51 @@
  *     TreeNode *right;
  *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
  * };
  */
 class Solution {
 private:
-void getparent(TreeNode* root ,unordered_map<TreeNode*,TreeNode*>&parent ){
-    if(root==NULL){
-        return;
-    }
-    if(root->left != NULL){
-        parent[root->left]=root;
-    }
-    if(root->right != NULL){
-        parent[root->right]=root;
-    }
-    getparent(root->left,parent);
-    getparent(root->right,parent);
-}
-void solve(TreeNode* root , unordered_map<TreeNode*,TreeNode*> &parent , int &ans){
-    if(root==NULL){
-        return;
-    }
-    if(parent[root] != NULL && parent.find(parent[root]) != parent.end() && parent[parent[root]] != NULL){
-        if(parent[parent[root]]->val %2 == 0){
-            ans = ans + root->val;
+    // void getparent(TreeNode* root ,unordered_map<TreeNode*,TreeNode*>&parent
+    // ){
+    //     if(root==NULL){
+    //         return;
+    //     }
+    //     if(root->left != NULL){
+    //         parent[root->left]=root;
+    //     }
+    //     if(root->right != NULL){
+    //         parent[root->right]=root;
+    //     }
+    //     getparent(root->left,parent);
+    //     getparent(root->right,parent);
+    // }
+    void solve(TreeNode* temp, int& sum) {
+        if (temp == NULL) {
+            return;
         }
+        if (temp->val % 2 == 0 && temp->left && temp->left->left)
+            sum = sum + (temp->left->left->val);
+
+        if (temp->val % 2 == 0 && temp->left && temp->left->right)
+            sum = sum + (temp->left->right->val);
+
+        if (temp->val % 2 == 0 && temp->right && temp->right->right)
+            sum = sum + (temp->right->right->val);
+
+        if (temp->val % 2 == 0 && temp->right && temp->right->left)
+            sum = sum + (temp->right->left->val);
+        solve(temp->left, sum);
+        solve(temp->right, sum);
     }
-    solve(root->left,parent,ans);
-    solve(root->right,parent,ans);
 
-
-}
 public:
     int sumEvenGrandparent(TreeNode* root) {
-        unordered_map<TreeNode*,TreeNode*> parent;
-        getparent(root,parent);
-        int ans = 0;
-        solve(root,parent,ans);
-        return ans;
+        // unordered_map<TreeNode*,TreeNode*> parent;
+        // getparent(root,parent);
+        int sum = 0;
+        solve(root, sum);
+        return sum;
     }
 };
