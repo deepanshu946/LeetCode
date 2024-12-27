ac7863ee8773f0bfbb5740bc0a3e9e881c5759fc
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2024-12-28 01:27:26 +0530

    Time: 262 ms (20.6%), Space: 118.6 MB (30.15%) - LeetHub

---
+++ 0952-word-subsets/0952-word-subsets.cpp
@@ -0,0 +1,54 @@
+class Solution {
+public:
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+        unordered_map<char,int> m1;
+        unordered_map<char,int> m2;
+        unordered_map<char,int> m3;
+        for(int i=0 ; i<words2[0].length() ; i++){
+            m1[words2[0][i]]++;
+        }
+        for(int i=1 ; i<words2.size(); i++){
+            string temp = words2[i];
+            for(int j=0 ; j<temp.length() ; j++){
+            m2[temp[j]]++;
+            }
+            for(auto &k:m2){
+                if(m1.find(k.first) != m1.end()){
+                    m1[k.first]=max(k.second,m1[k.first]);
+                }
+                else{
+                    m1[k.first]=k.second;
+                }
+            }
+            m2.clear();
+        }
+        for(auto i:m1){
+            cout<<i.first<<" "<<i.second<<endl;
+        }
+        vector<string> ans;
+        for(int i=0 ; i<words1.size() ; i++){
+            string temp = words1[i];
+            for(int j=0 ; j<temp.length() ; j++){
+                m3[temp[j]]++;
+            }
+            bool flag = 1;
+            for(auto k:m1){
+                if(m3.find(k.first) != m3.end()){
+                    if(m3[k.first] < k.second){
+                        flag = 0;
+                        break;
+                    }
+                }
+                else{
+                    flag=0;
+                    break;
+                }
+            }
+            if(flag ){
+                ans.push_back(temp);
+            }
+            m3.clear();
+        }
+        return ans;
+    }
+};
