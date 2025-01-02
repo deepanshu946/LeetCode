f9db042c68a228a724af9a893960fd566a25151b
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-01-02 16:44:44 +0530

    Time: 83 ms (90.13%), Space: 184.6 MB (46.35%) - LeetHub

--- 3442-maximum-total-reward-using-operations-i/3442-maximum-total-reward-using-operations-i.cpp
+++ 3442-maximum-total-reward-using-operations-i/3442-maximum-total-reward-using-operations-i.cpp
@@ -1,6 +1,6 @@
 class Solution {
 private:
-int solve(int i , vector<int>&nums , int n , int curr , map<pair<int,int> , int>  &dp){
+int solve(int i , vector<int>&nums , int n , int curr , vector<vector<int>> &dp){
     if(i>=n){
         return 0;
     }
@@ -8,15 +8,15 @@ int solve(int i , vector<int>&nums , int n , int curr , map<pair<int,int> , int>
     if(curr >= 2e3){
         return 0;
     }
-    if(dp.find({i,curr}) != dp.end()){
-        return dp[{i,curr}];
+    if(dp[i][curr] != -1){
+        return dp[i][curr];
     }
     int inc = 0;
     if(nums[i]>curr){
         inc = nums[i] + solve(i+1,nums,n,curr+nums[i],dp);
     }
    int  exc = solve(i+1,nums,n,curr,dp);
-    return dp[{i,curr}]= max(inc , exc);
+    return dp[i][curr]= max(inc , exc);
 }
 public:
     int maxTotalReward(vector<int>& nums) {
@@ -24,8 +24,8 @@ public:
         auto it = unique(nums.begin(), nums.end());
         nums.erase(it, nums.end());
 
-        // vector<int> dp(nums.size(),-1);
-        map<pair<int,int> , int > dp;
+        vector<vector<int>> dp(nums.size(),vector<int>(4001,-1));
+        // map<pair<int,int> , int > dp;
         return solve(0,nums,nums.size(),0,dp);
     }
 };
