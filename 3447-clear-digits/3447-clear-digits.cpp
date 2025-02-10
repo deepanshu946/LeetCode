b4ee5b0535ca3693a846b86b180e2f693438e542
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-02-10 17:27:53 +0530

    Time: 0 ms (100%), Space: 8.5 MB (70.42%) - LeetHub

---
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string ans;
+        for(int i=0 ; i<s.length() ; i++){
+            if(s[i] >= '0' && s[i]<='9'){
+                ans.pop_back();
+            }
+            else{
+                ans.push_back(s[i]);
+            }
+        }
+        return ans;
+    }
+};
