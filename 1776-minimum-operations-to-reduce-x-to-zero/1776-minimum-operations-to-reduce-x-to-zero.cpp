8dbabae8dacaf1b62e255d207ec0aa16fc55256e
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-03-14 02:47:59 +0530

    Time: 0 ms (100%), Space: 102.4 MB (39.97%) - LeetHub

---
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int minOperations(vector<int>& nums, int x) {
+        int sum = 0;
+        int n = nums.size();
+        for(int i=0 ; i<n ; i++){
+            sum += nums[i];
+        }
+        cout<<sum<<endl;
+        if(x>sum){
+            return -1;
+        }
+        if(sum==x){
+            return n;
+        }
+        int val = sum - x;
+        int left = 0;
+        int right = 0;
+        int len = 0;
+        int temp = 0;
+        while(right<n){
+            temp = temp + nums[right];
+            while(temp>val){
+                temp = temp - nums[left];
+                left++;
+            }
+            if(temp==val){
+                len = max(len , right-left+1);
+            }
+            right++;
+        }
+        if(len==0){
+            return -1;
+        }
+        return n - len;
+    }
+};
