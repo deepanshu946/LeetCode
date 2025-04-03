cd0ac7cebfd502cf73152a1ec249402a2302ab06
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-04-03 23:35:26 +0530

    Time: 0 ms (100%), Space: 13.7 MB (58.03%) - LeetHub

--- 1380-number-of-closed-islands/1380-number-of-closed-islands.cpp
+++ 1380-number-of-closed-islands/1380-number-of-closed-islands.cpp
@@ -36,7 +36,6 @@ public:
         for(int i=0 ; i<m ; i++){
             for(int j=0 ; j<n ; j++){
                 if(vis[i][j] == 0 && grid[i][j]==0){
-                    cout<<i<<" "<<j<<endl;
                     bool temp = solve(i,j,grid,vis,m,n);
                     if(temp){
                         ans++;
