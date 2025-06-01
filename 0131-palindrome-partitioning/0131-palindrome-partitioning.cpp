799015b3347fbcecc38f77a88588e3e84230a65b
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-06-01 18:55:02 +0530

    Time: 47 ms (60.52%), Space: 57.5 MB (61.75%) - LeetHub

---
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.cpp
@@ -0,0 +1,46 @@
+class Solution {
+    
+private:
+bool check(string str){
+    int i=0 ;
+    int j=str.length()-1;
+    while(i<=j){
+        if(str[i] != str[j]){
+            return false;
+        }
+        i++;
+        j--;
+    }
+    return true;
+}
+void solve(int start , int curr ,string s, vector<string> &temp, vector<vector<string>> &ans){
+    if(curr>=s.length()){
+        string subs = s.substr(start,curr-start+1);
+        if(subs.size() != 0 && check(subs)){
+            temp.push_back(subs);
+            ans.push_back(temp);
+            temp.pop_back();
+        }
+        if(subs.size()==0){
+            ans.push_back(temp);
+        }
+        // temp.clear();
+        return;
+    }
+    for(int i=start ; i<s.length() ; i++){
+        string subs = s.substr(start,i-start+1);
+        if(check(subs)){
+            temp.push_back(subs);
+            solve(i+1,i+1,s,temp,ans);
+            temp.pop_back();
+        }
+    }
+}
+public:
+    vector<vector<string>> partition(string s) {
+        vector<vector<string>> ans;
+        vector<string> temp;
+        solve(0,0,s,temp,ans);
+        return ans;
+    }
+};
