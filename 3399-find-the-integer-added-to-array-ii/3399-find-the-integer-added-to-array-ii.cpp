7e0ec7fb97ef369a38c1a9a752fafa53f69617d2
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-07-10 03:36:14 +0530

    Time: 411 ms (28.73%), Space: 425.6 MB (10.5%) - LeetHub

--- 3399-find-the-integer-added-to-array-ii/3399-find-the-integer-added-to-array-ii.cpp
+++ 3399-find-the-integer-added-to-array-ii/3399-find-the-integer-added-to-array-ii.cpp
@@ -3,6 +3,8 @@ public:
     int minimumAddedInteger(vector<int>& nums1, vector<int>& nums2) {
         int ans = INT_MAX;
         sort(nums2.begin(),nums2.end());
+        sort(nums1.begin(),nums1.end());
+
         for(int i=0 ; i<nums1.size() ; i++){
             for(int j=i+1 ; j<nums1.size() ; j++){
                 vector<int> temp;
@@ -11,7 +13,7 @@ public:
                         temp.push_back(nums1[k]);
                     }
                 }
-                sort(temp.begin(),temp.end());
+                // sort(temp.begin(),temp.end());
                 int diff = nums2[0]-temp[0];
                 bool check = 1;
                 for(int k=1 ; k<nums2.size() ; k++){
