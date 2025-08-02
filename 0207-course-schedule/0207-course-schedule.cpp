014d7cfd673e1176b022180b49749e35cec7886b
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-08-02 23:42:22 +0530

    Time: 7 ms (48.88%), Space: 19.1 MB (86.31%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool canFinish(int V, vector<vector<int>>& edges) {
+        vector<vector<int>> edge(V,vector<int>());
+        for(int i=0 ; i<edges.size() ; i++){
+            edge[edges[i][0]].push_back(edges[i][1]);
+        }
+        vector<int> indeg(V,0);
+        for(int i=0 ; i<edges.size() ; i++){
+            
+            indeg[edges[i][1]]++;
+            
+       
+        }
+        queue<int> q;
+        for(int i=0 ; i<indeg.size() ; i++){
+            if(indeg[i]==0){
+                q.push(i);
+            }
+            
+        }
+        // vector<int> ans;
+        int cnt = 0;
+        while(!q.empty()){
+            int t = q.front();
+            q.pop();
+            // ans.push_back(t);
+            cnt++;
+            for(auto it:edge[t]){
+                indeg[it]--;
+                if(indeg[it] == 0){
+                    q.push(it);
+                }
+            }
+        }
+        if(cnt==V){
+            return true;
+        }
+        return false;
+    }
+};
