8baa01becdd4caf59e9f57d2d5d282efbc337dc8
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-08-06 17:04:25 +0530

    Time: 83 ms (6.69%), Space: 72.8 MB (5.35%) - LeetHub

--- 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
+++ 0820-find-eventual-safe-states/0820-find-eventual-safe-states.cpp
@@ -24,22 +24,54 @@ bool dfs(int i , vector<vector<int>> &graph , vector<bool> &vis , vector<bool> &
 }
 public:
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        int n = graph.size();
-        vector<bool> vis(n,0);
-        vector<bool> pathvis(n,0);
-        vector<int> ans(n,0);
-        for(int i=0 ; i<n ; i++){
-            if(!vis[i]){
-                dfs(i,graph,vis,pathvis, ans);
+        // int n = graph.size();
+        // vector<bool> vis(n,0);
+        // vector<bool> pathvis(n,0);
+        // vector<int> ans(n,0);
+        // for(int i=0 ; i<n ; i++){
+        //     if(!vis[i]){
+        //         dfs(i,graph,vis,pathvis, ans);
+        //     }
+        // }
+        // vector<int> temp;
+        // for(int i=0 ; i<n ; i++){
+        //     if(ans[i]==1){
+        //         temp.push_back(i);
+        //     }
+        // }
+        // return temp;
+
+        //using topo sort
+        //reverse the edges and get all the nodes with indegree 0 and then removeal of edges on adjacent nodes
+
+        unordered_map<int,int> indegree;
+        queue<int> q;
+        vector<vector<int>> edge(graph.size(),  vector<int>());
+        for(int i=0 ; i<graph.size() ; i++){
+            indegree[i]+=graph[i].size();
+            if(indegree[i]==0){
+                q.push(i);
+            }
+            for(int j=0 ; j<graph[i].size() ; j++){
+                edge[graph[i][j]].push_back(i);
             }
         }
-        vector<int> temp;
-        for(int i=0 ; i<n ; i++){
-            if(ans[i]==1){
-                temp.push_back(i);
+        vector<int> ans;
+        while(!q.empty()){
+            int t = q.front();
+            q.pop();
+            ans.push_back(t);
+            for(auto it : edge[t]){
+                indegree[it]--;
+                if(indegree[it]==0){
+                    q.push(it);
+                }
             }
         }
-        return temp;
+        sort(ans.begin(),ans.end());
+        return ans;
+    
+        
 
     }
 };
