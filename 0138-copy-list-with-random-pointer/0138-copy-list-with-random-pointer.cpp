c86111e26ebbdcfb57013fc56620f2d4af681658
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-08-07 02:27:07 +0530

    Time: 7 ms (52.41%), Space: 15.1 MB (68.58%) - LeetHub

--- 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
+++ 0138-copy-list-with-random-pointer/0138-copy-list-with-random-pointer.cpp
@@ -15,66 +15,21 @@ public:
 */
 
 class Solution {
-private:
-void insertattail(Node* &head , Node* &tail , int data){
-    Node* temp = new Node(data);
-    if(head==NULL){
-        head = temp;
-        tail = temp;
-    }
-    else{
-        tail->next = temp;
-        tail = temp;
-    }
-}
 public:
     Node* copyRandomList(Node* head) {
-        Node* clonehead = NULL;
-        Node* clonetail = NULL;
-        Node* temp = head;
-        while(temp != NULL){
-            insertattail(clonehead , clonetail , temp->val );
-            temp = temp->next;
+        unordered_map<Node*,Node*> m;
+        Node* curr = head;
+        while(curr != NULL){
+            m[curr] = new Node(curr->val);
+            curr = curr->next;
         }
-        Node* originalhead = head;
-        Node* clonenode = clonehead;
-        while(originalhead != NULL  && clonehead != NULL){
-            Node* next = originalhead->next;
-            originalhead->next = clonenode;
-            originalhead= next;
-
-            Node* next2 = clonenode->next;
-            clonenode->next = originalhead;
-            clonenode = next2;
+        curr = head;
+        Node* newhead = m[curr];
+        while(curr != NULL){
+            m[curr]->next = m[curr->next];
+            m[curr]->random = m[curr->random];
+            curr = curr->next;
         }
-
-        originalhead = head;
-        
-
-        Node* temp2 = head;
-        while(temp2 != NULL){
-            if(temp2->random != NULL){
-                temp2->next->random = temp2->random->next;
-            }
-            else{
-                temp2->next->random = temp2->random;
-            }
-            
-            temp2 = temp2->next->next;
-        }
-        Node* ans = clonehead;
-
-
-        while(originalhead != NULL && clonehead !=NULL){
-            originalhead->next = clonehead->next;
-            originalhead = originalhead->next;
-            if(originalhead != NULL){
-            clonehead->next = originalhead->next;
-            clonehead = clonehead->next;
-            }
-            
-
-        }
-        return ans;
+        return newhead;
     }
 };
