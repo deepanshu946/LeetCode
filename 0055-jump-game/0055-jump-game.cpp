12f363f4feda13ff1b7517662371328970a85cbb
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-08-14 01:00:44 +0530

    Time: 515 ms (11.08%), Space: 54.8 MB (11.15%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -1,25 +1,32 @@
 class Solution {
 private:
-bool solve(vector<int> & nums , int n){
-    vector<bool> dp(n+1 , 0);
-    dp[n-1]=1;
-    for(int index = n-2 ; index>=0 ; index--){
-        bool ans = 0;
-        for(int i=1 ; i<=nums[index] ; i++){
-            if(index+i<=n){
-                ans = ans || dp[index+i];
-            }
-            else{
-                break;
-            }
-        }
-        dp[index] = ans ;
+bool solve(int i , vector<int> &nums){
+    if(i==nums.size()-1){
+        return 1;
     }
-    return dp[0];
+    if(i>=nums.size()){
+        return 0;
+    }
+
+    bool ans = 0;
+    for(int jump=1 ; jump<=nums[i] ; jump++){
+        ans = ans | solve(i+jump , nums);
+    }
+    return ans;
+
+
 }
 public:
     bool canJump(vector<int>& nums) {
-        int n = nums.size();
-        return solve(nums ,n);
+        vector<int> dp(nums.size() , 0);
+        dp[nums.size()-1]=1;
+        for(int i=nums.size()-2 ; i>=0 ; i--){
+             bool ans = 0;
+            for(int jump=1 ; jump<=nums[i] && i+jump<nums.size(); jump++){
+                ans = ans | dp[i+jump];
+            }
+            dp[i]=ans;
+        }
+        return dp[0];
     }
 };
