f3c8ce4962859777b131c1f17e21b748964811cc
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-08-14 22:53:29 +0530

    Time: 2596 ms (5.15%), Space: 93.6 MB (45.28%) - LeetHub

---
+++ 2504-find-all-good-indices/2504-find-all-good-indices.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<int> goodIndices(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> pre(nums.size(), 0);
+        for (int i = 1; i < n; i++) {
+            if (nums[i - 1] < nums[i]) {
+                pre[i] += pre[i - 1] + 1;
+            } else {
+                pre[i] = pre[i - 1];
+            }
+            cout << pre[i] << " ";
+        }
+        vector<int> suff(n, 0);
+        for (int i = n-2; i>=0; i--) {
+            if (nums[i+1] < nums[i]) {
+                suff[i] = suff[i + 1] + 1;
+            } else {
+                suff[i] = suff[i +1];
+            }
+            cout << suff[i] << " ";
+        }
+        vector<int> ans;
+        for(int i=k ; i<n-k ; i++){
+            if(pre[i-1]-pre[i-k] == 0 && suff[i+1]-suff[i+k]==0){
+                ans.push_back(i);
+            }
+        }
+        return ans;
+    }
+};
