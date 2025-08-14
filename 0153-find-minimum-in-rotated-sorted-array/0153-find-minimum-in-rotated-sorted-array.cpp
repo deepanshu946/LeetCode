22c0fbae6adba720e50853e6695d2d02558a2475
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-08-14 05:30:57 +0530

    Time: 0 ms (100%), Space: 14 MB (79.49%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -1,34 +1,29 @@
 class Solution {
 private:
-
+int solve(vector<int> &nums){
+    int start = 0;
+    int end = nums.size()-1;
+    while(start<=end){
+        int mid = (start+end)/2;
+        // if(nums[mid] < nums[mid+1] && nums[mid] < nums[mid-1]){
+        //     return mid;
+        // }
+        if(nums[mid] >= nums[0]){
+            start = mid+1;
+        }
+        else if(nums[mid] < nums[0]){
+            end = mid-1;
+        }
+        
+    }
+    return start;
+}
 public:
     int findMin(vector<int>& nums) {
-        int n = nums.size();
-        int start = 0;
-        int end = n-1;
-        if(nums[start] <= nums[end]){
-            return nums[start];
+        if(nums[0] <= nums[nums.size()-1]){
+            return nums[0];
         }
-        while(start<end){
-            int mid = (start+end)/2;
- 
-            if(mid==0 || mid==n-1){
-                return nums[n-mid-1];
-            }
-            else if(nums[mid]<nums[mid+1] && nums[mid]>nums[mid-1] && nums[mid]>nums[0]){
-                //1st line
-                start = mid;
-            }
-            else if(nums[mid]<nums[mid+1] && nums[mid]>nums[mid-1] && nums[mid]<nums[0]){
-                end = mid;
-            }
-            else if(nums[mid]>nums[mid+1] && nums[mid]>nums[mid-1]){
-                return nums[mid+1];
-            }
-            else if(nums[mid]<nums[mid+1] && nums[mid]<nums[mid-1]){
-                return nums[mid];
-            }
-        }
-        return  0;
+        int index = solve(nums);
+        return nums[index];
     }
 };
