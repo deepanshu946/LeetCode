db75f93849fd97b722c3296eaf71152929232f5b
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-08-14 05:43:21 +0530

    Time: 0 ms (100%), Space: 14.2 MB (12.53%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.cpp
@@ -20,7 +20,18 @@ public:
         if(nums[0] <= nums[nums.size()-1]){
             return nums[0];
         }
-        int index = solve(nums);
-        return nums[index];
+        int start = 0;
+    int end = nums.size()-1;
+    while(start<=end){
+        int mid = (start+end)/2;
+        if(nums[mid] >= nums[0]){
+            start = mid+1;
+        }
+        else if(nums[mid] < nums[0]){
+            end = mid-1;
+        }
+        
+    }
+    return nums[start]; 
     }
 };
