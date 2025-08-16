bcc5899988bac99c8f797141d2de2bca6fa07a83
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-08-16 18:47:42 +0530

    Time: 21 ms (66.32%), Space: 70.5 MB (45.82%) - LeetHub

--- 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1605-minimum-number-of-days-to-make-m-bouquets/1605-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,93 +1,46 @@
-// class Solution {
-// private:
-// bool check(vector<int>& bloom, int m, int k ,int day){
-//     int ans = 0;
-//     for(int i=0 ; i<bloom.size() ; i++){
-//         int count=0;
-//         while(i<bloom.size() && bloom[i]<=day && count<k){
-//             count++;
-//             i++;
-
-//         }
-        // if(count==k){
-        //     ans++;
-        //     i--;
-        // }
-//         if(ans==m){
-//             return true;
-//         }
-//     }
-//     return false;
-// }
-// public:
-//     int minDays(vector<int>& bloom, int m, int k) {
-//         if(bloom.size()<(long)m*k){
-//             return -1;
-//         }
-//         int maxi=INT_MIN ;
-//         int mini=INT_MAX ;
-//         for(int i=0 ; i<bloom.size() ; i++){
-//             mini = min(mini , bloom[i]);
-//             maxi = max(maxi,bloom[i]);
-//         }
-//         while(mini < maxi){
-//             int mid = (mini+maxi)/2;
-//             if(check(bloom,m,k,mid)){
-//                 maxi = mid;
-//             }
-//             else{
-//                 mini = mid+1;
-//             }
-//         }
-//         return mini;
-//     }
-// };
 class Solution {
-public:
-    int maxii(vector<int>& bloomDay) {
-        int max = INT_MIN;
-        for(int i=0;i<bloomDay.size();i++) {
-            if(bloomDay[i] > max) {
-                max = bloomDay[i];
+private:
+bool check(int mid, vector<int> &nums , int k , int m){
+    int count=0;
+    int temp=0;
+    for(int i=0 ; i<nums.size() ; i++){
+        if(nums[i]<=mid){
+            count++;
+            if(count==k){
+                temp++;
+                count=0;
             }
         }
-        return max; 
-    }
-    
-    long long FindFlowers(vector<int>& bloom, int m, int k ,int day){
-    int ans = 0;
-    for(int i=0 ; i<bloom.size() ; i++){
-        int count=0;
-        while(i<bloom.size() && bloom[i]<=day && count<k){
-            count++;
-            i++;
+        else{
+            count=0;
         }
-         if(count==k){
-            ans++;
-            i--;
-        }
-    
     }
-    return ans;
+    return temp>=m;
 }
+public:
+    int minDays(vector<int>& nums, int m, int k) {
+        int mini = INT_MAX;
+        int maxi = INT_MIN;
+        for(int i=0 ; i<nums.size() ; i++){
+            mini = min(mini , nums[i]);
+            maxi = max(maxi , nums[i]);
 
-    int minDays(vector<int>& bloomDay, int m, int k) {
-       if(bloomDay.size()/k < m) return -1;
-        int l = 1;
-        int r = maxii(bloomDay);
-        long long flowers; int ans = 0;
-        int mid;
-        while(l <= r) {
-           mid = l + (r-l)/2;
-           flowers = FindFlowers(bloomDay, m, k, mid);
-           if(flowers < m) {
-             l = mid + 1;
-           }
-           else if(flowers >= m){
-            ans = mid;
-            r = mid - 1;
-           }
         }
-        return ans;
+        int start = mini;
+        int end = maxi;
+        while(start<=end){
+            int mid = (start+end)/2;
+            if(check(mid,nums,k,m)){
+                end = mid-1;
+
+            }
+            else{
+                start = mid+1;
+            }
+        }
+        if(start>maxi){
+            return -1;
+        }
+        return start;
     }
 };
