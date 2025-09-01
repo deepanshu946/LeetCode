ea9da0961dd1304d19c27d2123f78d3361e257c6
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-09-02 03:14:49 +0530

    Time: 5 ms (27.38%), Space: 61.7 MB (65.45%) - LeetHub

---
+++ 1690-maximum-length-of-subarray-with-positive-product/1690-maximum-length-of-subarray-with-positive-product.cpp
@@ -0,0 +1,63 @@
+class Solution {
+public:
+    int getMaxLen(vector<int>& nums) {
+        int ans = 0;
+        int negcount = 0;
+        int totalcount = 0;
+        int firstneg =0;
+        int lastneg=0;
+        int start = 0;
+        int end = 0;
+        bool f = 0;
+        for(int i=0 ; i<nums.size() ; i++){
+            if(nums[i]==0){
+                end--;
+                if(negcount%2==0){
+                    ans = max(ans , totalcount);
+                }
+                // else if(end-start == 1 && totalcount==1 && negcount==1){
+                //     // continue;
+                // }
+                else {
+                    ans = max(ans , max(end-firstneg,lastneg-start));
+                }
+                negcount = 0;
+                totalcount = 0;
+                start = end = i+1;
+                firstneg=lastneg=0;
+                f=0;
+            }
+            else{
+                if(nums[i]<0){
+                    negcount++;
+                    if(f==0){
+                        firstneg = i;
+                        f=1;
+                    }
+                    lastneg = i;
+
+                }
+                totalcount++;
+                end++;
+            }
+        }
+        end--;
+        cout<<totalcount<<endl;
+        cout<<negcount<<endl;
+        cout<<firstneg<<endl;
+        cout<<lastneg<<endl;
+        cout<<start<<endl;
+        cout<<end<<endl;
+
+        if(negcount%2==0){
+            ans = max(ans , totalcount);
+        }
+        else if(end-start == 1 && totalcount==1 && negcount==1){
+                    // continue;
+                }
+        else{
+            ans = max(ans , max(end-firstneg,lastneg-start));
+        }
+        return ans;
+    }
+};
