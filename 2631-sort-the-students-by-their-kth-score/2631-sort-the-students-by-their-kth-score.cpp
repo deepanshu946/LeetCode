195ce5e3c97e4f0267be5b9c74a6e03636e9904e
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2025-10-05 02:01:29 +0530

    Time: 0 ms (100%), Space: 32.3 MB (31.76%) - LeetHub

---
+++ 2631-sort-the-students-by-their-kth-score/2631-sort-the-students-by-their-kth-score.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> sortTheStudents(vector<vector<int>>& score, int k) {
+        vector<pair<int,int>> temp;
+        for(int i=0 ; i<score.size() ; i++){
+            temp.push_back({score[i][k],i});
+        }
+        sort(temp.begin(),temp.end());
+        reverse(temp.begin(),temp.end());
+        vector<vector<int>> ans(score.size(),vector<int>(score[0].size(),0));
+        for(int i=0 ; i<temp.size() ; i++){
+            ans[i]=score[temp[i].second];
+        }
+        return ans;
+    }
+};
