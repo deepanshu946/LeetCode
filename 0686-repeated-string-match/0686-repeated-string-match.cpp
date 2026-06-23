66a3369d8bfeedf2fdadb5d1868820b525384e8b
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-06-23 22:07:26 +0530

    Time: 61 ms (12.16%), Space: 154.2 MB (7.97%) - LeetHub

---
+++ 0686-repeated-string-match/0686-repeated-string-match.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int repeatedStringMatch(string a, string b) {
+        string temp = a;
+        int count = 1;
+        while(a.length() < b.length()){
+            a = a + temp;
+            count++;
+        } 
+        if(a.contains(b)){
+            return count;
+        }
+        a = a+temp;
+        if(a.contains(b) ){
+            return count+1;
+        }
+        return -1;
+    }
+};
