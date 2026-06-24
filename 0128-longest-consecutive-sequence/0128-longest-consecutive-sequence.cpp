2b57ed74981ccfb62da7a79d1cd8d1555256ad13
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-06-24 22:24:45 +0530

    Time: 91 ms (22.14%), Space: 92.9 MB (6.15%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.cpp
@@ -1,49 +1,26 @@
 class Solution {
-private:
-
 public:
     int longestConsecutive(vector<int>& nums) {
+        unordered_map<int,int> m;
+        unordered_set<int> s(nums.begin(),nums.end());
 
-        // map<int,bool> m;
-        // for(int i=0 ; i<nums.size() ; i++){
-        //     m[nums[i]]=1;
-        // }
-        // unordered_map<int,int> m2;
-        // int ans = 0;
-        // for(auto i:m){
-        //     int val = i.first;
-        //     if(m.find(val-1) == m.end()){
-        //         m2[val]=1;
-        //     }
-        //     else{
-        //         //val-1 present in nums
-        //         m2[val]=1+m2[val-1];
-        //     }
-
-        // ans = max(ans,m2[val]);
-
-        // }
-        // return ans;
-
-        unordered_map<int,bool> m;
-        for(int i=0 ; i<nums.size() ; i++){
-            m[nums[i]]=1;
-        }
 
         int ans = 0;
-
-        for(auto i:m){
-            int val = i.first;
-            // int temp = 0;
-            if(m.find(val-1) == m.end()){
-                int count=1;
-                int temp = val+1;
-                while(m.find(temp) != m.end()){
-                    count++;
-                    temp++;
-                }
-                ans=max(ans,count);
+        for(int i=0 ; i<nums.size() ; i++){
+            if(s.find(nums[i]-1) == s.end()){
+                m[nums[i]]=1;
+                // ans = max(ans , 1);
             }
+        }
+        for(auto i:m){
+            int num = i.first;
+            int temp = 0;
+
+            while(s.find(num) != s.end()){
+                temp++;
+                num++;
+            }
+            ans = max(ans , temp);
         }
         return ans;
     }
