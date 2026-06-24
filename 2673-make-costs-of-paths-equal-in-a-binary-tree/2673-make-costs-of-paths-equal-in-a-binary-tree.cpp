55b8bee4c45a5c938a3ee9119b271c28c826ba51
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-06-25 03:15:53 +0530

    Time: 3271 ms (5.26%), Space: 145.8 MB (11.96%) - LeetHub

---
+++ 2673-make-costs-of-paths-equal-in-a-binary-tree/2673-make-costs-of-paths-equal-in-a-binary-tree.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int minIncrements(int n, vector<int>& cost) {
+        int start = (n/2) - 1;
+        int ans = 0;
+        // unordered_map<int,int> m;
+        vector<int> m(n,-1);
+        for(int i=start ; i>=0 ; i--){
+            int firstch = (i*2) + 1;
+            int secondch = (i*2) + 2;
+            cout<<firstch<<" "<<secondch<<endl;
+            int left = 0;
+            int right = 0;
+            if(m[firstch] == -1){
+                left = cost[firstch];
+                right = cost[secondch];
+            }
+            else{
+                left = cost[firstch]+m[firstch];
+                right = cost[secondch]+m[secondch];
+            }
+            ans = ans + abs(left-right);
+            cout<<max(left,right)<<endl;
+            m[i]=max(left,right);
+        }
+        return ans;
+    }
+};
