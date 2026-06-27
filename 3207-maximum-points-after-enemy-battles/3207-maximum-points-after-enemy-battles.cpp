e2bb8529ba32beaba9f623e4edad732b1e2b8fc3
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-06-27 15:22:51 +0530

    Time: 0 ms (100%), Space: 76.8 MB (12.14%) - LeetHub

--- 3207-maximum-points-after-enemy-battles/3207-maximum-points-after-enemy-battles.cpp
+++ 3207-maximum-points-after-enemy-battles/3207-maximum-points-after-enemy-battles.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
     long long maximumPoints(vector<int>& nums, int curr) {
-        sort(nums.begin(), nums.end());
-        if(curr<nums[0]){
+        // sort(nums.begin(), nums.end());
+        long long sum = 0;
+        int mini = INT_MAX;
+        for(int i=0; i<nums.size() ; i++){
+            sum = sum + nums[i];
+            mini = min(mini,nums[i]);
+        }
+        if(curr<mini){
             return 0;
         }
-        curr = curr - nums[0];
-        long long sum = 0;
-        for(int i=1 ; i<nums.size() ; i++){
-            sum = sum + nums[i];
-        }
+        sum = sum - mini;
+        curr = curr - mini;
+        
         sum = curr + sum;
-        return 1+(sum/nums[0]);
+        return 1+(sum/mini);
     }
 };
