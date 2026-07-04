56306b55288ecd2f5bb58ae45a2fdc0b2a160075
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-07-05 04:24:43 +0530

    Time: 503 ms (63.22%), Space: 429.7 MB (42.85%) - LeetHub

--- 3655-xor-after-range-multiplication-queries-ii/3655-xor-after-range-multiplication-queries-ii.cpp
+++ 3655-xor-after-range-multiplication-queries-ii/3655-xor-after-range-multiplication-queries-ii.cpp
@@ -1,77 +1,67 @@
 #define mod 1000000007
-
 class Solution {
+
 public:
-    long long power(long long a, long long b) {
-        long long res = 1;
-        while (b) {
-            if (b & 1) res = (res * a) % mod;
-            a = (a * a) % mod;
-            b >>= 1;
-        }
-        return res;
+    long long power(long long a, long long b){
+    if(b==0){
+        return 1;
     }
-
-    long long modInverse(long long x) {
-        return power(x, mod - 2);
+    long long res = 1;
+    while(b){
+        if(b&1) res = (res*a)%mod;
+        a = (a*a)%mod;
+        b >>= 1;
     }
+    return res;
+}
 
+    long long modInverse(long long x){
+        return power(x, mod-2);
+    }
     int xorAfterQueries(vector<int>& nums, vector<vector<int>>& queries) {
-
         int n = nums.size();
-        int block = sqrt(n) + 1;
-
-        unordered_map<int, vector<vector<int>>> mp;
-
-        // Large k -> brute force
-        for (auto &q : queries) {
-            int l = q[0], r = q[1], k = q[2], v = q[3];
-
-            if (k >= block) {
-                for (int i = l; i <= r; i += k)
-                    nums[i] = (1LL * nums[i] * v) % mod;
-            } else {
-                mp[k].push_back(q);
-            }
-        }
-
-        // Small k
-        for (auto &it : mp) {
-
-            int k = it.first;
-
-            vector<long long> diff(n, 1);
-
-            for (auto &q : it.second) {
-
-                int l = q[0];
-                int r = q[1];
-                long long v = q[3] % mod;
-
-                diff[l] = (diff[l] * v) % mod;
-
-                int nxt = l + ((r - l) / k + 1) * k;
-
-                if (nxt < n)
-                    diff[nxt] = (diff[nxt] * modInverse(v)) % mod;
-            }
-
-            // Process each remainder independently
-            for (int rem = 0; rem < k; rem++) {
-
-                long long cur = 1;
-
-                for (int i = rem; i < n; i += k) {
-                    cur = (cur * diff[i]) % mod;
-                    nums[i] = (1LL * nums[i] * cur) % mod;
+        int block = (sqrt(n))+1;
+        unordered_map<int,vector<vector<int>> > m;
+        for(int i=0 ;i<queries.size() ; i++){
+            int k= queries[i][2];
+            if(k>=block){
+                for(int j=queries[i][0] ; j<=queries[i][1] ; j+=k){
+                    nums[j] =(1LL* nums[j]*queries[i][3])%mod;
                 }
             }
+            else{
+                m[queries[i][2]].push_back(queries[i]);
+            }
+
         }
+        for(auto& i:m){
+            vector<long long> diff(nums.size(),1);
+            int k = i.first;
+            // cout<<k<<endl;
+            for(auto &j:i.second){
+                diff[j[0]] = (diff[j[0]] * j[3]) % mod;
+                int steps = (j[1]-j[0])/k;
+                int next = j[0]+((steps+1)*k);
+                if(next<nums.size()){
+                    diff[next] = (diff[next] * power(j[3],mod-2)) % mod;
+                }
+            }
+            for(int x=0 ; x<nums.size() ; x++){
+                if(x-k>=0){
+                    diff[x] = (diff[x] * diff[x-k]) % mod;
+                }
+                
+            }
+            for(int x=0; x<n ; x++){
+                nums[x] = (1LL * nums[x] * diff[x]) % mod;
+            }
 
+
+        }
         int ans = 0;
-        for (int x : nums)
-            ans ^= x;
-
+        for(int i=0 ;i<nums.size() ;i++){
+            ans = ans ^ nums[i];
+        }
         return ans;
     }
 };
