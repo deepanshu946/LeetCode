b8a3b57a5cccb2019ff2b4bd770f8a8fe87d26a0
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-07-05 01:55:37 +0530

    Time: 73 ms (56.71%), Space: 197.3 MB (99.53%) - LeetHub

---
+++ 3824-minimum-k-to-reduce-array-within-limit/3824-minimum-k-to-reduce-array-within-limit.cpp
@@ -0,0 +1,47 @@
+class Solution {
+private:
+bool checknums(vector<int> &nums , long long k){
+    if(k==0){
+        return false;
+    }
+    long long count = 0;
+    for(int i=0 ; i<nums.size() ; i++){
+        int rem = nums[i]%k;
+        count += nums[i]/k;
+        if(rem!=0){
+            count++;
+        }
+    }
+    if(count <= k*k){
+        return true;
+    }
+    return false;
+}
+int solve(int start , int end , vector<int> &nums){
+    int s = start;
+    int e = 1e5;
+    int ans = 0;
+    while(s<=e){
+        long long mid = (s+e)/2;
+        bool check = checknums(nums,mid);
+        if(check){
+            ans = mid;
+            e = mid-1;
+        }
+        else{
+            s = mid+1;
+        }
+    }
+    return ans;
+
+}
+public:
+    int minimumK(vector<int>& nums) {
+        int maxi = 0;
+        for(int i=0 ; i<nums.size(); i++){
+            maxi = max(maxi,nums[i]);
+        }
+        return solve(0,maxi,nums);
+
+    }
+};
