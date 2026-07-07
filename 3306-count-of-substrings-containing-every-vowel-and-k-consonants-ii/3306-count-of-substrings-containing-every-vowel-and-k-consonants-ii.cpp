71def3b028819ea985e9f3862e1c7c560bbd1f97
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-07-07 21:14:38 +0530

    Time: 187 ms (45.27%), Space: 79.3 MB (5.64%) - LeetHub

---
+++ 3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii/3306-count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -0,0 +1,67 @@
+
+class Solution {
+private:
+bool checkvovel(char ch){
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+        return true;
+    }
+    return false;
+}
+public:
+    long long countOfSubstrings(string word, int k) {
+        vector<long long> temp(word.length(),0);
+        // if(checkvovel(word[word.length()-1])){
+        //     temp[word.length()-1]=1;
+        // }
+        for(int i=word.length()-2; i>=0 ; i--){
+            if(checkvovel(word[i+1])){
+                temp[i] = temp[i+1]+1;
+            }
+            else{
+                temp[i]=0;
+            }
+
+        }
+        unordered_map<int,long long> vovels;
+        long long count = 0;
+        int left = 0;
+        int right = 0;
+        long long ans = 0;
+        while(right<word.length()){
+            if(checkvovel(word[right])){
+                vovels[word[right]]++;
+            }
+            else{
+                count++;
+            }
+            while(count>k){
+                if(checkvovel(word[left])){
+                    vovels[word[left]]--;
+                    if(vovels[word[left]]==0){
+                        vovels.erase(word[left]);
+                    }
+                }
+                else{
+                    count--;
+                }
+                left++;
+            }
+            while(vovels.size()==5 && count==k){
+                ans = ans+ temp[right]+1;
+                if(checkvovel(word[left])){
+                    vovels[word[left]]--;
+                    if(vovels[word[left]]==0){
+                        vovels.erase(word[left]);
+                    }
+                }
+                else{
+                    count--;
+                }
+                left++;
+            }
+            right++;
+        }
+        return ans;
+
+    }
+};
