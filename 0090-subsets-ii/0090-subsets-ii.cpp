458c07c4cacb61934c32d93c3aa95bdc897a2603
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-07-17 02:23:48 +0530

    Time: 3 ms (32.12%), Space: 10.5 MB (50.9%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,35 +1,26 @@
 class Solution {
-private:
-void solve(int i , set<vector<int>> &s , vector<int> &nums , vector<int> &temp){
-    if(i>=nums.size()){
-        // sort(temp.begin(),temp.end());
-        s.insert(temp);
-        // temp.clear();
-        return;
-    }
-    temp.push_back(nums[i]);
-    solve(i+1,s,nums,temp);
-    temp.pop_back();
-    solve(i+1,s,nums,temp);
-}
 public:
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        set<vector<int>> s;
-        vector<int> temp;
-        solve(0,s,nums ,temp);
         vector<vector<int>> ans;
-        set<vector<int>> s2;
+        sort(nums.begin(),nums.end());
+        int n = nums.size();
+        for(int mask = 0 ; mask<(1<<n) ; mask++){
+            vector<int> temp;
+            bool check=0;
+            for(int i=0 ; i<n ; i++){
+                if(mask & (1<<i)){
+                    if(i>0 && nums[i]==nums[i-1] && !(mask&(1<<(i-1)))){
+                        check=1;
+                        break;
+                    }
+                    temp.push_back(nums[i]);
+                }
 
-        for(auto i:s){
-            vector<int> temp2 =i;
-            sort(temp2.begin(),temp2.end());
-            // s.erase(i);
-            s2.insert(temp2);
-        }
-        for(auto i:s2){
-            ans.push_back(i);
+            }
+            if(!check){
+                ans.push_back(temp);
+            }
         }
         return ans;
-        
     }
 };
