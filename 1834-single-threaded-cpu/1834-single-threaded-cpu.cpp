f49a545a690f6df7d1487f8bf2130f493a587e4c
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-07-21 21:51:04 +0530

    Time: 380 ms (5.91%), Space: 355.6 MB (5.03%) - LeetHub

---
+++ 1834-single-threaded-cpu/1834-single-threaded-cpu.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    vector<int> getOrder(vector<vector<int>>& tasks) {
+        vector<int> ans;
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        vector<vector<int>> temp;
+        for(int i=0 ; i<tasks.size() ; i++){
+            temp.push_back({tasks[i][0],tasks[i][1],i});
+
+        }
+        sort(temp.begin(),temp.end());
+        long long curr = temp[0][0];
+        int i = 1;
+        pq.push({temp[0][1],temp[0][2]});
+        while(i<temp.size() && temp[i][0]<=curr){
+            pq.push({temp[i][1],temp[i][2]});
+            i++;
+        }
+        while(ans.size()<tasks.size()){
+            if(pq.empty()){
+                curr = temp[i][0];
+            }
+            while(i<temp.size() && temp[i][0]<=curr){
+                pq.push({temp[i][1],temp[i][2]});
+                i++;
+            }
+            curr = curr + pq.top().first;
+            ans.push_back(pq.top().second);
+            pq.pop();
+            while(i<temp.size() && temp[i][0]<=curr){
+                pq.push({temp[i][1],temp[i][2]});
+                i++;
+            }
+            
+            
+        }
+        return ans;
+
+    }
+};
