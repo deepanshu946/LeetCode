d618498a2db3b876ba28184ffc814b7a166d7e5c
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-08-16 22:59:27 +0530

    Time: 23 ms (51.07%), Space: 102.9 MB (78.26%) - LeetHub

--- 0739-daily-temperatures/0739-daily-temperatures.cpp
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -1,46 +1,21 @@
-// class Solution {
-// public:
-//     vector<int> dailyTemperatures(vector<int>& temperatures) {
-//         int n = temperatures.size();
-//         vector<int> ans(n);
-//         stack<pair<int,int>> s;
-//         s.push({0,0});
-//         int index = n-1;
-//         for(index ; index>=0 ; index--){
-//             int val = temperatures[index];
-//             while(s.top().first != 0 && s.top().first <= val){
-//                 s.pop();
-//             }
-//             if(s.top().first ==0 ){
-//                 ans[index]=0;
-//             }
-//             else{
-//                 ans[index]=s.top().second - index;
-//             }
-//             s.push(make_pair(val,index));
-//         }
-//         return ans;
-//     }
-// };
 class Solution {
 public:
-    vector<int> dailyTemperatures(vector<int>& temperatures) {
-        int n=temperatures.size();
-        vector<int> ans(n,0);
-        stack<int> st;
-        // st.push(n-1);
-        for(int i=temperatures.size()-1;i>=0;i--)
-        {
-            while(!st.empty() && temperatures[i] >= temperatures[st.top()])
-            {
+    vector<int> dailyTemperatures(vector<int>& nums) {
+        stack<pair<int,int>> st;
+        vector<int> ans(nums.size(),0);
+        for(int i=nums.size()-1 ; i>=0 ; i--){
+            while(!st.empty() && st.top().first <= nums[i]){
                 st.pop();
             }
-            if(!st.empty()){
-                ans[i]=st.top() - i;
+            if(st.empty()){
+                ans[i]=0;
             }
-            st.push(i);
+            else{
+                ans[i]=st.top().second - i;
+
+            }
+            st.push({nums[i],i});
         }
-     
         return ans;
     }
 };
