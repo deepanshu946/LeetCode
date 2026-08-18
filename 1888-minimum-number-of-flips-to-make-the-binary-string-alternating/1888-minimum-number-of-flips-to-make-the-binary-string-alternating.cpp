f6d1cd1e6a1a259df73e3903f3fdd4b4d90b6fb5
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-08-18 23:00:33 +0530

    Time: 10 ms (78.29%), Space: 17.3 MB (60.17%) - LeetHub

--- 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
+++ 1888-minimum-number-of-flips-to-make-the-binary-string-alternating/1888-minimum-number-of-flips-to-make-the-binary-string-alternating.cpp
@@ -3,45 +3,83 @@ public:
     int minFlips(string s) {
         int count1 = 0; //1 se start me kitne changes
         int count2 = 0; // 0 se start me kitne changes
+        
+        char start1 = '1';
+        char start2 = '0';
         for(int i=0 ; i<s.length() ; i++){
-            char expected1 = (i % 2 == 0) ? '1' : '0';
-            char expected2 = (i % 2 == 0) ? '0' : '1';
-            if(s[i] != expected1){
+            if(s[i]!=start1){
                 count1++;
             }
-            if(s[i] != expected2){
+            if(s[i] != start2){
                 count2++;
             }
+            if(start1 == '1'){
+                start1 = '0';
+            }
+            else{
+                start1 = '1';
+            }
+            if(start2 == '1'){
+                start2 = '0';
+            }
+            else{
+                start2 = '1';
+            }
         }
-        int n = s.length();
+
         int ans = min(count1,count2);
         s = s+s;
         // cout<<s<<endl;
+        int n = s.length();
         int left = 0;
-        int right = n ;
+        int right = (n/2) ;
+        char prev1 = '1';
+        char prev2 = '0';
 
-        while(right < s.length()){
-            char expected1 = (right % 2 == 0) ? '1' : '0';
-            char expected2 = (right % 2 == 0) ? '0' : '1';
-            char prevexpected1 = (left % 2 == 0) ? '1' : '0';
-            char prevexpected2 = (left % 2 == 0) ? '0' : '1';
-            if(s[right]!=expected1){
+        while(right < n){
+            
+            if(s[right]!=start1){
                 count1++;
             }
-            if(s[right] != expected2){
+            if(s[right] != start2){
                 count2++;
             }
-            if(s[left] != prevexpected1){
+            if(s[left] != prev1){
                 count1--;
             }
-            if(s[left] != prevexpected2){
+            if(s[left] != prev2){
                 count2--;
+            }
+            if(start1 == '1'){
+                start1 = '0';
+            }
+            else{
+                start1 = '1';
+            }
+            if(start2 == '1'){
+                start2 = '0';
+            }
+            else{
+                start2 = '1';
+            }
+            if(prev1 == '1'){
+                prev1 = '0';
+            }
+            else{
+                prev1 = '1';
+            }
+            if(prev2 == '1'){
+                prev2 = '0';
+            }
+            else{
+                prev2 = '1';
             }
             ans = min(ans , min(count1, count2));
             left++;
             right++;
 
         }
+        // ans = min(ans , min(count1,count2));
         return ans;
         
     }
