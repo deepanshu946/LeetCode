17dfe97a98e3cd240dfeb3f50254fd286ededf9b
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-08-19 17:46:39 +0530

    Time: 30 ms (32.8%), Space: 139.3 MB (26.36%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.cpp
@@ -1,19 +1,33 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        multiset<int> s;
+        deque<int> pq;
         for(int i=0 ; i<k ; i++){
-            s.insert(nums[i]);
+            if(pq.size()==0){
+                pq.push_back(i);
+            }
+            else{
+                while(!pq.empty() && nums[pq.back()]< nums[i]){
+                    pq.pop_back();
+                }
+                pq.push_back(i);
+            }
         }
         int left = 0;
-
         vector<int> ans;
-        ans.push_back(*s.rbegin());
-        for(int i=k ; i<nums.size() ; i++){
-            s.erase(s.find(nums[left]));
-            s.insert(nums[i]);
+        ans.push_back(nums[pq.front()]);
+        int right = k;
+        while(right < nums.size()){
+            if(pq.front() <= left){
+                pq.pop_front();
+            }
+            while(!pq.empty() && nums[pq.back()]< nums[right]){
+                pq.pop_back();
+            }
+            pq.push_back(right);
+            ans.push_back(nums[pq.front()]);
+            right++;
             left++;
-            ans.push_back(*s.rbegin());
         }
         return ans;
     }
