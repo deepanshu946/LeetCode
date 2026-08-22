df5600e8587cf4e77e2d5e02e50dc95ce1fb3ba1
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-08-23 03:21:15 +0530

    Time: 1132 ms (5.63%), Space: 11.9 MB (12.87%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -1,8 +1,8 @@
 class Solution {
 private:
-void solve(vector<vector<char>>& board, string word,bool &ans , int i , int j , int m , int n , vector<vector<bool>> &vis,string temp){
+void solve(vector<vector<char>>& board, string word,bool &ans , int i , int j , int m , int n , vector<vector<bool>> &vis,string temp,int count){
     temp.push_back(board[i][j]);
-    if(temp.length()>word.length()){
+    if(temp.length()>word.length() || word[count] != board[i][j]){
         return;
     }
     if(temp == word){
@@ -11,16 +11,16 @@ void solve(vector<vector<char>>& board, string word,bool &ans , int i , int j , 
     }
     vis[i][j]=1;
     if(i+1 < m && !vis[i+1][j] ){
-        solve(board,word,ans,i+1,j,m,n,vis,temp);
+        solve(board,word,ans,i+1,j,m,n,vis,temp,count+1);
     }
     if(j+1 < n && !vis[i][j+1] ){
-        solve(board,word,ans,i,j+1,m,n,vis,temp);
+        solve(board,word,ans,i,j+1,m,n,vis,temp,count+1);
     }
     if(i-1 >=0  && !vis[i-1][j] ){
-        solve(board,word,ans,i-1,j,m,n,vis,temp);
+        solve(board,word,ans,i-1,j,m,n,vis,temp,count+1);
     }
     if(j-1 >=0 && !vis[i][j-1] ){
-        solve(board,word,ans,i,j-1,m,n,vis,temp);
+        solve(board,word,ans,i,j-1,m,n,vis,temp,count+1);
     }
     vis[i][j]=0;
     temp.pop_back();
@@ -36,7 +36,7 @@ public:
         for(int i=0 ; i<m ; i++){
             for(int j=0 ; j<n ; j++){
                 if(board[i][j]==word[0]){
-                    solve(board,word,ans,i,j,m,n,vis,temp);
+                    solve(board,word,ans,i,j,m,n,vis,temp,0);
                     if(ans){
                         return true;
                     }
