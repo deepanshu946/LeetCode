275daf93f229efed0289ac3b9bbb9ac39043a2fd
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-08-30 00:33:40 +0530

    Time: 490 ms (24.73%), Space: 296.5 MB (22.28%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,53 +1,20 @@
 class Solution {
-// private:
-// int solve(vector<int> &nums , int n){
-//     // vector<vector<int>> dp(n+1 , vector<int>(n+1,0));
-//     vector<int> nextrow(n+1 , 0);
-//     vector<int> currrow(n+1,0);
-//     // if(curr==n){
-//     //     return 0;
-//     // }
-//     // if(dp[curr][prev+1] !=-1){
-//     //     return dp[curr][prev+1];
-//     // }
-//     for(int curr = n-1 ; curr>=0 ; curr--){
-//         for(int prev = n-1 ; prev>=-1 ; prev--){
-//             int inc =0;
-//             int exc=0;
-//             if(prev == -1 || nums[curr]>nums[prev]){
-//                 inc = 1+nextrow[curr+1];
-//             }
-//             exc = nextrow[prev+1];
-//             currrow[prev+1]=max(inc,exc);
-//             // return dp[curr][prev];
-//         }
-//         nextrow = currrow;
-//     }
-//     return nextrow[0];
-// }
 public:
-    int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        // vector<vector<int>> dp(n ,vector<int>(n+1,-1));
-        // return solve(nums,n);
-        
-
-
-        //using dp binary search
-        if(n==0){
-            return 0;
-        }
-        vector<int> ans;
-        ans.push_back(nums[0]);
-        for(int i=1 ; i<n ; i++){
-            if(nums[i]>ans.back()){
-                ans.push_back(nums[i]);
+    int lengthOfLIS(vector<int>& A) {
+         int n = A.size();
+    // return solve(0,A,A.size(),0);
+    vector<vector<int>> dp(n+1 , vector<int>(n+1,0));
+    for(int i=n-1 ; i>=0 ; i--){
+        for(int prev = n ; prev >=0 ; prev--){
+            int inc=0;
+            if( prev-1 == -1 || A[i]>A[prev-1]){
+                inc = 1 + dp[i+1][i+1];
             }
-            else{
-                int index = lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
-                ans[index]=nums[i];
-            }
+            int exc =dp[i+1][prev];
+            dp[i][prev]= max(inc , exc);
         }
-        return ans.size();
+    }
+    // cout<<dp[0][0]<<endl;
+    return dp[0][0];
     }
 };
