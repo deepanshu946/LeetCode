7206fce163c3bc25fac6f751e3ef148bc0b86d8a
Deepanshu Agarwal <deepanshuagarwal946@gmail.com>
2026-09-01 23:39:35 +0530

    Time: 1494 ms (5%), Space: 58.3 MB (5.02%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -2,70 +2,65 @@ class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
         unordered_map<string,vector<string>> m;
-        for(int i=0 ; i<wordList.size() ; i++){
+        for(int i=0 ; i< wordList.size() ; i++){
+            string word = wordList[i];
             for(int j=0 ; j<wordList.size() ; j++){
-                if(i==j){
-                    continue;
-                }
-                int p=0;
-                int q=0;
-                int count=0;
-                while(p<wordList[j].length()){
-                    if(wordList[i][p] != wordList[j][q]){
-                        count++;
+                string word2 = wordList[j];
+                int change = 0;
+                for(int k=0;k<word.length() ; k++){
+                    if(word[k] != word2[k]){
+                        change++;
                     }
-                    if(count>=2){
+                    if(change>1){
                         break;
                     }
-                    p++;
-                    q++;
                 }
-                if(count==1){
-                    m[wordList[i]].push_back(wordList[j]);
+                if(change == 1){
+                    m[word].push_back(word2);
+                    m[word2].push_back(word);
+
                 }
             }
         }
+        string word = beginWord;
         for(int j=0 ; j<wordList.size() ; j++){
-                int p=0;
-                int q=0;
-                int count=0;
-                while(p<wordList[j].length()){
-                    if(wordList[j][p] != beginWord[q]){
-                        count++;
-                    }
-                    if(count>=2){
-                        break;
-                    }
-                    p++;
-                    q++;
+            string word2 = wordList[j];
+            int change = 0;
+            for(int k=0;k<word.length() ; k++){
+                if(word[k] != word2[k]){
+                    change++;
                 }
-                if(count==1){
-                    m[beginWord].push_back(wordList[j]);
+                if(change>1){
+                    break;
                 }
             }
-        queue<string> q;
-        unordered_set<string> s;
-        int count=0;
-        q.push(beginWord);
-        s.insert(beginWord);
-        // count = 0;
-        while(!q.empty()){
-            int size = q.size();
-            count++;
+            if(change == 1){
+                m[word].push_back(word2);
+                // m[word2].push_back(word);
 
-            for(int i=0 ; i<size ; i++){
-                string top = q.front();
-                q.pop();
-                if(top==endWord){
-                    return count;
+            }
+        }
+        unordered_set<string> vis;
+        queue<string> pq;
+        pq.push(beginWord);
+        vis.insert(beginWord);
+        int steps = 1;
+        while(!pq.empty()){
+            int size = pq.size();
+            while(size--){
+                string s = pq.front();
+                pq.pop();
+                if(s == endWord){
+                    return steps;
                 }
-                for(auto it : m[top]){
-                    if(s.find(it) == s.end()){
-                        s.insert(it);
-                        q.push(it);
+                for(auto i:m[s]){
+                    if(vis.find(i) == vis.end()){
+                        pq.push(i);
+                        vis.insert(i);
                     }
                 }
             }
+            steps++;
         }
         return 0;
     }
